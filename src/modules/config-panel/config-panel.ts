cc7d1d49904665860e1f169f0b81272b812e33b4
bastian schnittger <bastian.schnittger@5minds.de>
2017-12-15 15:50:08 +0100

    add precursor for settings

--- src/modules/config-panel/config-panel.ts
+++ src/modules/config-panel/config-panel.ts
@@ -1,7 +1,7 @@
 import {ConsumerClient, IUserTaskConfig} from '@process-engine/consumer_client';
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
-import * as toastr from 'toastr';
+import environment from '../../environment';
 
 @inject(Router, 'ConsumerClient')
 export class ConfigPanel {
@@ -9,9 +9,22 @@ export class ConfigPanel {
   private router: Router;
   private consumerClient: ConsumerClient;
 
+  public config: any = environment.consumerClient;
+
   constructor(router: Router, consumerClient: ConsumerClient) {
     this.router = router;
     this.consumerClient = consumerClient;
+    this.config.baseRoute = environment.consumerClient.baseRoute;
+  }
+
+  public updateSettings(): void {
+    environment.consumerClient.baseRoute = this.config.baseRoute;
+    window.localStorage.setItem('baseRoute', this.config.baseRoute);
+    this.consumerClient.updateConfig(this.config);
+  }
+
+  public getSettings(): void {
+
   }
 
 }
