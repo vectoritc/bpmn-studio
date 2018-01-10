e701a97b9bc19e888ee8daaff7ddc67b8d787902
Alex <alexander.kasten@5minds.de>
2018-01-10 15:30:24 +0100

    :bug: Fix glyphicons and change start script

--- bin/charon.js
+++ bin/charon.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
 const server = require('node-http-server');
+const config = new server.Config;
 const open = require('open');
-const argv = require('minimist')(process.argv.slice(2));
 
-argv['root'] = __dirname + '/..';
+config.root = __dirname + '/..';
+config.contentType.woff2 = 'application/font-woff2';
+config.contentType.woff = 'application/font-woff';
+config.contentType.ttf = 'application/x-font-ttf';
 
-server.deploy(argv, (result) => {
+server.deploy(config, (result) => {
   const url = `http://localhost:${result.config.port}/`;
   console.log(`Charon started at ${url}`);
   console.log(`Press CRTL+C to exit.`);
