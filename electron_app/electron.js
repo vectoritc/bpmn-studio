f6ba06114cddad5719d479830e03bd03bda3ceed
bastian schnittger <bastian.schnittger@5minds.de>
2018-01-18 16:20:46 +0100

    add autoupdate functionallity

--- electron_app/electron.js
+++ electron_app/electron.js
@@ -1,5 +1,7 @@
 const electron = require('electron');
+const {autoUpdater} = require('electron-updater');
 const app = electron.app;
+const notifier = require('electron-notifications');
 
 let mainWindow = null;
 function createWindow () {
@@ -8,10 +10,47 @@ function createWindow () {
   }
 
   mainWindow = new electron.BrowserWindow({width: 800, height: 600});
+
   mainWindow.loadURL(`file://${__dirname}/../index.html`);
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
+
+  autoUpdater.checkForUpdates();
+
+  autoUpdater.addListener('error', (error) => {
+    const notification = notifier.notify('Update error', {
+      message: 'Update failed',
+      buttons: ['Dismiss'],
+    });
+    notification.on('buttonClicked', (text, buttonIndex, options) => {
+      notification.close();
+    });
+  });
+
+  autoUpdater.addListener('update-available', (info) => {
+    notifier.notify('Update available', {
+      message: 'Started downloading',
+    });
+  });
+
+  autoUpdater.addListener('update-downloaded', (info) => {
+    const notification = notifier.notify('Update ready', {
+      message: 'Update ready for installation',
+      duration: '60000',
+      buttons: ['Install', 'Dismiss'],
+    });
+
+    notification.on('buttonClicked', (text, buttonIndex, options) => {
+      if (text == 'Install') {
+        autoUpdater.quitAndInstall();
+      } else {
+        notification.close();
+      }
+    })
+  });
+
+
 }
 
 app.on('ready', createWindow);
