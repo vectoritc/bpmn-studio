6c641c99dc62b2bdb4b5227ff5459b6ae6526114
Alex <alexander.kasten@5minds.de>
2018-01-22 16:05:46 +0100

    :sparkles: :construction: WIP Add addMessage button and function

--- src/contracts/bpmnmodeler/IBpmnModdle.ts
+++ src/contracts/bpmnmodeler/IBpmnModdle.ts
@@ -4,4 +4,5 @@ import {IModdleElement} from './IModdleElement';
 export interface IBpmnModdle {
   fromXML(xml: string, callback: (err: Error, definitions: IDefinition) => void): void;
   create(name: string, attributes: any): IModdleElement;
+  toXML(definitions: IDefinition, callback: (err: Error, xmlStrUpdated: string) => void): void;
 }
--- src/modules/property-panel/indextaps/general/sections/message-event/message-event.ts
+++ src/modules/property-panel/indextaps/general/sections/message-event/message-event.ts
@@ -1,41 +1,37 @@
 import {IBpmnModdle,
   IBpmnModeler,
-  IBpmnModelerConstructor,
   IDefinition,
   IEvent,
   IEventBus,
   IModdleElement,
-  IModeling,
   IPageModel,
-  ISection,
-  IShape} from '../../../../../../contracts';
+  ISection} from '../../../../../../contracts';
+
+import {bindable, observable} from 'aurelia-framework';
 
 export class MessageEventSection implements ISection {
 
   public path: string = '/sections/message-event/message-event';
   public canHandleElement: boolean = false;
 
-  private elementInPanel: IShape;
   private businessObjInPanel: IModdleElement;
   private eventBus: IEventBus;
-  private modeling: IModeling;
   private moddle: IBpmnModdle;
-  private xml: string;
+  private modeler: IBpmnModeler;
+  private msgDropdown: HTMLSelectElement;
 
-  public messages: Array<IModdleElement>;
-  public selectedId: string;
+  @observable public messages: Array<IModdleElement>;
+  @bindable public selectedId: string;
   public selectedMessage: IModdleElement;
 
   public async activate(model: IPageModel): Promise<void> {
     this.eventBus = model.modeler.get('eventBus');
-    this.modeling = model.modeler.get('modeling');
     this.moddle = model.modeler.get('moddle');
+    this.modeler = model.modeler;
 
-    this.xml = await this.getXML(model.modeler);
-    this.messages = await this.getMessages(this.moddle);
+    this.messages = await this.getMessages();
 
     this.eventBus.on('element.click', (event: IEvent) => {
-      this.elementInPanel = event.element;
       this.businessObjInPanel = event.element.businessObject;
 
       if (this.businessObjInPanel.eventDefinitions && this.businessObjInPanel.eventDefinitions[0].messageRef) {
@@ -46,21 +42,18 @@ export class MessageEventSection implements ISection {
     });
   }
 
-  private getXML(modeler: IBpmnModeler): Promise<string> {
-    return new Promise((resolve: Function, reject: Function): void => {
-
-      let xml: string;
-      modeler.saveXML({format: true}, (err: Error, diagrammXML: string) => {
-        xml = diagrammXML;
-      });
-      resolve(xml);
+  private getXML(): string {
+    let xml: string;
+    this.modeler.saveXML({format: true}, (err: Error, diagrammXML: string) => {
+      xml = diagrammXML;
     });
+    return xml;
   }
 
-  private getMessages(moddle: IBpmnModdle): Promise<Array<IModdleElement>> {
+  private getMessages(): Promise<Array<IModdleElement>> {
     return new Promise((resolve: Function, reject: Function): void => {
 
-      moddle.fromXML(this.xml, (err: Error, definitions: IDefinition) => {
+      this.moddle.fromXML(this.getXML(), (err: Error, definitions: IDefinition) => {
         const rootElements: Array<IModdleElement> = definitions.get('rootElements');
         const messages: Array<IModdleElement> = rootElements.filter((element: IModdleElement) => {
           return element.$type === 'bpmn:Message';
@@ -91,4 +84,32 @@ export class MessageEventSection implements ISection {
   private updateName(): void {
     this.businessObjInPanel.eventDefinitions[0].messageRef.name = this.selectedMessage.name;
   }
+
+  private async addMessage(): Promise<void> {
+    this.moddle.fromXML(this.getXML(), (err: Error, definitions: IDefinition) => {
+      const bpmnMessage: IModdleElement = this.moddle.create('bpmn:Message', { id: `Message_${this.random()}`, name: 'Message Name' });
+      definitions.get('rootElements').push(bpmnMessage);
+      this.moddle.toXML(definitions, (error: Error, xmlStrUpdated: string) => {
+        this.modeler.importXML(xmlStrUpdated, (errr: Error) => {
+         this.refreshMessages();
+         return 0;
+        });
+      });
+    });
+  }
+
+  private async refreshMessages(): Promise<void> {
+    this.messages = await this.getMessages();
+  }
+
+  private random(): string {
+    let random: string = '';
+    const possible: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i: number = 0; i < 8; i++) {
+      random += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return random;
+  }
+
 }
