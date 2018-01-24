354b5c60b0f1a3b552ee58faee1dd561671bb3b1
bastian schnittger <bastian.schnittger@5minds.de>
2018-01-24 09:48:35 +0100

    add error handling for no element selected

--- src/modules/bpmn-io/bpmn-io.ts
+++ src/modules/bpmn-io/bpmn-io.ts
@@ -78,10 +78,12 @@ export class BpmnIo {
 
     const selectedElements: Array<IModdleElement> = this.modeler.get('selection')._selectedElements;
 
-    modeling.setColor(selectedElements, {
-      fill: fillColor,
-      stroke: strokeColor,
-    });
+    if (selectedElements.length > 0) {
+      modeling.setColor(selectedElements, {
+        fill: fillColor,
+        stroke: strokeColor,
+      });
+    }
   }
 
 }
