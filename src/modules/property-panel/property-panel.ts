007fccc83e4a586c0c5c8fd11e7ac38a6f74e26b
Alex <alexander.kasten@5minds.de>
2018-02-05 10:03:45 +0100

    :fire: Remove setFirstElement from propertypanel

--- src/modules/property-panel/property-panel.ts
+++ src/modules/property-panel/property-panel.ts
@@ -23,40 +23,16 @@ export class PropertyPanel {
   private currentRegister: any = this.generalRegister;
   private registers: Array<any>;
 
-  private attached(): void {
+  public attached(): void {
     this.registers = [
       this.generalRegister,
       this.formsRegister,
       this.extensionsRegister,
     ];
-    this.setFirstElement();
   }
 
   public updateLabel(selectedRegister: any): void {
     this.currentRegister = selectedRegister;
   }
 
-  private setFirstElement(): void {
-    this.moddle = this.modeler.get('moddle');
-
-    this.moddle.fromXML(this.getXML(), async(err: Error, definitions: IDefinition) => {
-      const rootElements: any = definitions.get('rootElements');
-      const process: IModdleElement = rootElements.find((element: any) => {
-        return element.$type === 'bpmn:Process';
-      });
-
-      const elementRegistry: IElementRegistry = this.modeler.get('elementRegistry');
-      const elementInPanel: IShape = elementRegistry.get(process.flowElements[0].id);
-
-      this.modeler.get('selection').select(elementInPanel);
-    });
-  }
-
-  private getXML(): string {
-    let xml: string;
-    this.modeler.saveXML({format: true}, (err: Error, diagrammXML: string) => {
-      xml = diagrammXML;
-    });
-    return xml;
-  }
 }
