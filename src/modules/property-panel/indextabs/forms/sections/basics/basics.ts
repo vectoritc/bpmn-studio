e624d5e69fa5d48c103d79d1cd7680ededa096a2
bastian schnittger <bastian.schnittger@5minds.de>
2018-02-16 23:40:03 +0100

    rework 2 formstab

--- src/modules/property-panel/indextabs/forms/sections/basics/basics.ts
+++ src/modules/property-panel/indextabs/forms/sections/basics/basics.ts
@@ -19,7 +19,6 @@ export class BasicsSection implements ISection {
   private isFormSelected: boolean = false;
 
   private businessObjInPanel: IFormElement;
-  private eventBus: IEventBus;
   private moddle: IBpmnModdle;
   private modeler: IBpmnModeler;
 
@@ -34,21 +33,12 @@ export class BasicsSection implements ISection {
   private activeListElementId: string;
 
   public async activate(model: IPageModel): Promise<void> {
-    this.eventBus = model.modeler.get('eventBus');
+    this.businessObjInPanel = model.elementInPanel.businessObject;
+
     this.moddle = model.modeler.get('moddle');
     this.modeler = model.modeler;
-    const selectedEvents: Array<IShape> = this.modeler.get('selection')._selectedElements;
-    if (selectedEvents[0]) {
-      this.businessObjInPanel = selectedEvents[0].businessObject;
-      this.init();
-    }
 
-    this.eventBus.on(['element.click', 'shape.changed', 'selection.changed'], (event: IEvent) => {
-      if (event.element) {
-        this.businessObjInPanel = event.element.businessObject;
-      }
-      this.init();
-    });
+    this.init();
   }
 
   public checkElement(element: IShape): boolean {
