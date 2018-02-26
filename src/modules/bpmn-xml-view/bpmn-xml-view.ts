18bc6012a401e8f1e2a8bade938d385f110c4ca1
Alex <alexander.kasten@5minds.de>
2018-02-26 17:12:20 +0100

    :construction: WIP Add xml view, now using highlightjs

--- src/modules/bpmn-xml-view/bpmn-xml-view.ts
+++ src/modules/bpmn-xml-view/bpmn-xml-view.ts
@@ -1,4 +1,8 @@
+// import 'ace-builds';
 import {bindable} from 'aurelia-framework';
+import {DOM} from 'aurelia-pal';
+import * as hljs from 'highlight.js';
+import 'highlightjs-line-numbers.js';
 // import {Prism} from 'prismjs';
 import 'prismjs';
 // import * as lineNumbers from 'prismjs/plugins/line-numbers/prism-line-numbers.js';
@@ -8,8 +12,17 @@ export class BpmnXmlView {
 
   @bindable() public xml: string;
 
+  @bindable public test: HTMLElement;
+  public code: HTMLElement;
+
   public attached(): void {
     this.xml = beautify(this.xml);
+    hljs.initHighlighting();
+    hljs.initHighlighting.called = false; // tslint:disable-line
+    hljs.initLineNumbersOnLoad(); // tslint:disable-line
+    // const editor: any = ace.edit('editor');
+    // editor.setTheme('ace/theme/twilight');
+    // editor.session.setMode('ace/mode/javascript');
   }
 
 }
