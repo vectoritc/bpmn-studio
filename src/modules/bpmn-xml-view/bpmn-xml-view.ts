cfc76fe09978daf0620def658e1aab3dc95867ae
Alex <alexander.kasten@5minds.de>
2018-02-28 10:28:57 +0100

    :bug: Fix indentation bug

--- src/modules/bpmn-xml-view/bpmn-xml-view.ts
+++ src/modules/bpmn-xml-view/bpmn-xml-view.ts
@@ -3,42 +3,22 @@ import * as hljs from 'highlight.js';
 import 'highlightjs-line-numbers.js';
 import * as beautify from 'xml-beautifier';
 
-interface InitHighlighting {
-  (): void;
-  called: boolean;
-}
-
 const highlightEngine: {
-  initHighlighting: InitHighlighting;
   // tslint:disable-next-line:prefer-method-signature
-  initLineNumbersOnLoad: () => void;
+  highlightBlock: (element: HTMLElement) => void;
   // tslint:disable-next-line:prefer-method-signature
-  lineNumbersBlock: (element: any) => void;
-  // tslint:disable-next-line:prefer-method-signature
-  highlightBlock: (element: any) => void;
+  lineNumbersBlock: (element: HTMLElement) => void;
 } = hljs as any;
 
 export class BpmnXmlView {
 
+  private codeElement: HTMLElement;
   @bindable() public xml: string;
 
   public attached(): void {
     this.xml = beautify(this.xml);
-    highlightEngine.highlightBlock(document.getElementById('test'));
-    highlightEngine.configure({
-      classPrefix: 'hljs-',
-      tabReplace: true,
-      useBR: false,
-      languages: undefined,
-    });
-    // highlightEngine.initHighlighting.called = false;
-    // highlightEngine.initLineNumbersOnLoad();
-    highlightEngine.lineNumbersBlock(document.getElementById('test'));
-  }
-
-  public detached(): void {
-    console.log('detached called');
-    this.xml = null;
+    highlightEngine.highlightBlock(this.codeElement);
+    highlightEngine.lineNumbersBlock(this.codeElement);
   }
 
 }
