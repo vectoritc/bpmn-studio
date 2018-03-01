aa1c5438c9a9b35ffc9a0d333d8c7a56364f413b
bastian schnittger <bastian.schnittger@5minds.de>
2018-03-01 11:31:36 +0100

    bump version

--- electron_app/electron.js
+++ electron_app/electron.js
@@ -25,8 +25,8 @@ function createWindow () {
   }
 
   mainWindow = new electron.BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     title: "BPMN-Studio",
   });
 
