b5f576bb511b0344e8d75a1be15a1d4d5b4f98d6
bastian schnittger <bastian.schnittger@5minds.de>
2018-03-08 13:06:25 +0100

    add routing capability to call activity

--- src/modules/property-panel/indextabs/general/sections/call-activity/call-activity.ts
+++ src/modules/property-panel/indextabs/general/sections/call-activity/call-activity.ts
@@ -1,5 +1,6 @@
 import {IPagination, IProcessDefEntity} from '@process-engine/consumer_client';
 import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
 import {
   ICallActivityElement,
   IPageModel,
@@ -8,19 +9,22 @@ import {
 } from '../../../../../../contracts';
 import {GeneralService} from '../../service/general.service';
 
-@inject(GeneralService)
+@inject(GeneralService, Router)
 export class CallActivitySection implements ISection {
 
   public path: string = '/sections/call-activity/call-activity';
   public canHandleElement: boolean = false;
 
   public allProcesses: IPagination<IProcessDefEntity>;
+  public selectedId: string;
 
   private businessObjInPanel: ICallActivityElement;
   private generalService: GeneralService;
+  private router: Router;
 
-  constructor(generalService: GeneralService) {
+  constructor(generalService?: GeneralService, router?: Router) {
     this.generalService = generalService;
+    this.router = router;
   }
 
   public async activate(model: IPageModel): Promise<void> {
@@ -34,6 +38,10 @@ export class CallActivitySection implements ISection {
                                         && element.businessObject.$type === 'bpmn:CallActivity';
 
     return elementIsCallActivity;
+  }
+
+  public navigateToCalledProcess(): void {
+    this.router.navigate(`/processdef/${this.selectedId}/detail`);
   }
 
   private clearCalledElement(): void {
