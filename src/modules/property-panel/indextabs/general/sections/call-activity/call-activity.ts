f33dca1deaa3e58b2fe765f7a3c4ae257aaa15a3
bastian schnittger <bastian.schnittger@5minds.de>
2018-03-08 10:43:51 +0100

    add and use new service function in call activity

--- src/modules/property-panel/indextabs/general/sections/call-activity/call-activity.ts
+++ src/modules/property-panel/indextabs/general/sections/call-activity/call-activity.ts
@@ -1,19 +1,31 @@
+import {IPagination, IProcessDefEntity} from '@process-engine/consumer_client';
+import {inject} from 'aurelia-framework';
 import {
   ICallActivityElement,
   IPageModel,
   ISection,
   IShape,
 } from '../../../../../../contracts';
+import {GeneralService} from '../../service/general.service';
 
+@inject(GeneralService)
 export class CallActivitySection implements ISection {
 
   public path: string = '/sections/call-activity/call-activity';
   public canHandleElement: boolean = false;
 
+  public allProcesses: IPagination<IProcessDefEntity>;
+
   private businessObjInPanel: ICallActivityElement;
+  private generalService: GeneralService;
+
+  constructor(generalService: GeneralService) {
+    this.generalService = generalService;
+  }
 
   public async activate(model: IPageModel): Promise<void> {
     this.businessObjInPanel = model.elementInPanel.businessObject;
+    this._getAllProcesses();
   }
 
   public isSuitableForElement(element: IShape): boolean {
@@ -26,5 +38,9 @@ export class CallActivitySection implements ISection {
 
   private clearCalledElement(): void {
     this.businessObjInPanel.calledElement = '';
+  }
+
+  private async _getAllProcesses(): Promise<void> {
+    this.allProcesses = await this.generalService.getAllProcesses();
   }
 }
