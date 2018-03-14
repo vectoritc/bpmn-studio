cb90408b23503d0c4a7f82e3f799df9f82542e69
bastian schnittger <bastian.schnittger@5minds.de>
2018-03-14 09:57:16 +0100

    correct indent

--- src/contracts/eventbus/IEventBus.ts
+++ src/contracts/eventbus/IEventBus.ts
@@ -1,26 +1,24 @@
 export interface IEventBus {
-    on(events: Array<string> | string,
+  on(events: Array<string> | string,
+     priority: number,
+     callback: Function,
+     callbackScope?: any): void;
+  on(events: Array<string> | string,
+     callback: Function,
+     callbackScope?: any): void;
+
+  once(events: Array<string> | string,
        priority: number,
        callback: Function,
-       callbackScope?: any): void;
-     on(events: Array<string> | string,
-        callback: Function,
-        callbackScope?: any): void;
-
-     once(events: Array<string> | string,
-          priority: number,
-          callback: Function,
-          callbackScope: any): void;
-     once(events: Array<string> | string,
-          callback: Function,
-          callbackScope: any): void;
-
-     off(event: string,
-         callback?: Function): void;
-
-     fire(name: string,
-          data?: any): any;
-     // tslint:disable-next-line:unified-signatures
-     fire(eventObject: {type: string},
-          data?: any): any;
+       callbackScope: any): void;
+  once(events: Array<string> | string,
+       callback: Function,
+       callbackScope: any): void;
+  off(event: string,
+      callback?: Function): void;
+  fire(name: string,
+       data?: any): any;
+  // tslint:disable-next-line:unified-signatures
+  fire(eventObject: {type: string},
+       data?: any): any;
 }
