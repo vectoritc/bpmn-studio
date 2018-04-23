bfaeb698d7695c86a0d9f62dc2f03de3299cc106
Alex <alexander.kasten@5minds.de>
2018-04-23 12:50:52 +0200

    :sparkles: :fire: Use notificationService instead toastr on config-panel

--- src/modules/config-panel/config-panel.ts
+++ src/modules/config-panel/config-panel.ts
@@ -1,21 +1,24 @@
 import {BpmnStudioClient, IUserTaskConfig} from '@process-engine/bpmn-studio_client';
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
-import * as toastr from 'toastr';
 import environment from '../../environment';
+import {NotificationType} from './../../contracts/index';
+import {NotificationService} from './../notification/notification.service';
 
-@inject(Router, 'BpmnStudioClient')
+@inject(Router, 'BpmnStudioClient', 'NotificationService')
 export class ConfigPanel {
 
   private router: Router;
   private bpmnStudioClient: BpmnStudioClient;
+  private notificationService: NotificationService;
 
   public config: any = environment.bpmnStudioClient;
 
-  constructor(router: Router, bpmnStudioClient: BpmnStudioClient) {
+  constructor(router: Router, bpmnStudioClient: BpmnStudioClient, notificationService: NotificationService) {
     this.router = router;
     this.bpmnStudioClient = bpmnStudioClient;
     this.config.baseRoute = environment.bpmnStudioClient.baseRoute;
+    this.notificationService = notificationService;
   }
 
   public updateSettings(): void {
@@ -28,12 +31,12 @@ export class ConfigPanel {
     environment.processengine.routes.startProcess = `${this.config.baseRoute}/processengine/start`;
     environment.processengine.routes.userTasks =  `${this.config.baseRoute}/datastore/UserTask`;
     this.bpmnStudioClient.updateConfig(this.config);
-    toastr.success('Sucessfully saved settings!');
+    this.notificationService.showNotification(NotificationType.SUCCESS, 'Sucessfully saved settings!');
     this.router.navigate('');
   }
 
   public cancelUpdate(): void {
-    toastr.warning('Settings dismissed!');
+    this.notificationService.showNotification(NotificationType.WARNING, 'Settings dismissed!');
     this.router.navigate('');
   }
 
