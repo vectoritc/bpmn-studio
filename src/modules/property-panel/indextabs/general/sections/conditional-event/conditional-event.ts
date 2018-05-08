1231b1258f7fead01fce8c26e256585f98dc00d2
Alex <alexander.kasten@5minds.de>
2018-05-08 15:05:07 +0200

    :lipstick: Improve Code Structure

--- src/modules/property-panel/indextabs/general/sections/conditional-event/conditional-event.ts
+++ src/modules/property-panel/indextabs/general/sections/conditional-event/conditional-event.ts
@@ -11,31 +11,26 @@ export class ConditionalEventSection implements ISection {
   public path: string = '/sections/conditional-event/conditional-event';
   public canHandleElement: boolean = false;
 
-  private businessObjInPanel: IModdleElement;
-  private moddle: IBpmnModdle;
-  private conditionObject: IModdleElement;
-
   public conditionBody: string;
   public variableName: string;
   public variableEvent: string;
 
+  private _businessObjInPanel: IModdleElement;
+  private _moddle: IBpmnModdle;
+  private _conditionObject: IModdleElement;
+
   public activate(model: IPageModel): void {
-    this.businessObjInPanel = model.elementInPanel.businessObject;
+    this._moddle = model.modeler.get('moddle');
+    this._businessObjInPanel = model.elementInPanel.businessObject;
 
-    this.moddle = model.modeler.get('moddle');
-
-    this._init();
-  }
-
-  private _init(): void {
-    const {variableName, variableEvent, condition} = this.businessObjInPanel.eventDefinitions[0];
+    const {variableName, variableEvent, condition} = this._businessObjInPanel.eventDefinitions[0];
 
     this.variableEvent = (variableEvent === undefined) ? '' : variableEvent;
     this.variableName = (variableName === undefined) ? '' : variableName;
     this.conditionBody = (condition === undefined) ? '' : condition.body;
 
-    this.conditionObject = this.moddle.create('bpmn:FormalExpression', {body: this.conditionBody});
-    this.businessObjInPanel.eventDefinitions[0].condition = this.conditionObject;
+    this._conditionObject = this._moddle.create('bpmn:FormalExpression', {body: this.conditionBody});
+    this._businessObjInPanel.eventDefinitions[0].condition = this._conditionObject;
   }
 
   public isSuitableForElement(element: IShape): boolean {
@@ -46,15 +41,15 @@ export class ConditionalEventSection implements ISection {
   }
 
   public updateCondition(): void {
-    this.businessObjInPanel.eventDefinitions[0].condition.body = this.conditionBody;
+    this._businessObjInPanel.eventDefinitions[0].condition.body = this.conditionBody;
   }
 
   public updateVariableName(): void {
-    this.businessObjInPanel.eventDefinitions[0].variableName = this.variableName;
+    this._businessObjInPanel.eventDefinitions[0].variableName = this.variableName;
   }
 
   public updateVariableEvent(): void {
-    this.businessObjInPanel.eventDefinitions[0].variableEvent = this.variableEvent;
+    this._businessObjInPanel.eventDefinitions[0].variableEvent = this.variableEvent;
   }
 
   public clearCondition(): void {
