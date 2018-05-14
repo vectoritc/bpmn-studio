0708276c5f154619da0d13fe505ec78c3d13f870
Alex <alexander.kasten@5minds.de>
2018-05-14 16:17:54 +0200

    :sparkles: Change Default Port and Check for Port Argument

--- bin/bpmn-studio.js
+++ bin/bpmn-studio.js
@@ -4,11 +4,22 @@ const server = require('node-http-server');
 const config = new server.Config;
 const open = require('open');
 
+const defaultPort = 17290;
+
 config.root = __dirname + '/..';
 config.contentType.woff2 = 'application/font-woff2';
 config.contentType.woff = 'application/font-woff';
 config.contentType.ttf = 'application/x-font-ttf';
 config.contentType.svg = 'image/svg+xml';
+
+for (let count = 0; count < process.argv.length; count++) {
+  if (process.argv[count].includes('port=')) {
+    config.port = process.argv[count].substr(5);
+    break;
+  } else {
+    config.port = defaultPort;
+  }
+}
 
 server.deploy(config, (result) => {
   const url = `http://localhost:${result.config.port}/`;
