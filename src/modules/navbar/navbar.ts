59e8c90333e2af82f549346f02cdf2c9640edcc1
bastian schnittger <bastian.schnittger@5minds.de>
2018-05-14 12:02:33 +0200

    add eventaggregator to listen to navigation events

--- src/modules/navbar/navbar.ts
+++ src/modules/navbar/navbar.ts
@@ -1,3 +1,45 @@
-export class NavBar {
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {inject} from 'aurelia-framework';
+import {RouteConfig, Router} from 'aurelia-router';
 
+@inject(Router, EventAggregator)
+export class NavBar {
+  private router: Router;
+  private eventAggregator: EventAggregator;
+
+  public activeRouteTitle: string;
+
+  constructor(router: Router, eventAggregator: EventAggregator) {
+    this.router = router;
+    this.eventAggregator = eventAggregator;
+  }
+
+  public attached(): void {
+    this.dertermineActiveRoute();
+    this.eventAggregator.subscribe('router:navigation:complete', () => {
+      this.dertermineActiveRoute();
+    });
+  }
+
+  public navigate(routeTitle: string): void {
+    const route: RouteConfig = this.router.routes.find((r: RouteConfig) => {
+      return r.title === routeTitle;
+    });
+    this.router.navigate(`/${route.route}`);
+  }
+
+  private isRouteActive(routeTitle: string): boolean {
+    if (this.router.currentInstruction.config.title === routeTitle) {
+      return true;
+    }
+    return false;
+  }
+
+  private dertermineActiveRoute(): void {
+    this.router.routes.forEach((route: RouteConfig) => {
+      if (this.isRouteActive(route.title)) {
+        this.activeRouteTitle = route.title;
+      }
+    });
+  }
 }
