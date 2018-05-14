92628156bad7ff861ce5d158720cd95912fde417
bastian schnittger <bastian.schnittger@5minds.de>
2018-05-14 17:45:31 +0200

    add navbar as singleton

---
+++ src/modules/navbar/index.ts
@@ -0,0 +1,8 @@
+import {FrameworkConfiguration} from 'aurelia-framework';
+import {NavBar} from './navbar';
+
+export async function configure(config: FrameworkConfiguration): Promise<void> {
+  config.container.registerSingleton('Navbar', NavBar);
+  console.log(NavBar);
+  await config.container.get('AuthenticationService').initialize();
+}
