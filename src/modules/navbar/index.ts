bc1a77a0a98d19b4148e65073c7c8ee5fc8bbece
bastian schnittger <bastian.schnittger@5minds.de>
2018-05-15 09:17:30 +0200

    kill container approach

--- src/modules/navbar/index.ts
+++
@@ -1,7 +0,0 @@
-import {FrameworkConfiguration} from 'aurelia-framework';
-import {NavBar} from './navbar';
-
-export async function configure(config: FrameworkConfiguration): Promise<void> {
-  config.container.registerSingleton('Navbar', NavBar);
-  await config.container.get('AuthenticationService').initialize();
-}
