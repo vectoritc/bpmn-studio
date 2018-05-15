c9c2cc9e48f966d76b3aa928429b47757e9a7235
bastian schnittger <bastian.schnittger@5minds.de>
2018-05-15 15:36:16 +0200

    add update baseRoute functionallity

--- src/modules/config-panel/config-panel.ts
+++ src/modules/config-panel/config-panel.ts
@@ -1,24 +1,27 @@
 import {BpmnStudioClient, IUserTaskConfig} from '@process-engine/bpmn-studio_client';
+import {EventAggregator} from 'aurelia-event-aggregator';
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import environment from '../../environment';
 import {NotificationType} from './../../contracts/index';
 import {NotificationService} from './../notification/notification.service';
 
-@inject(Router, 'BpmnStudioClient', 'NotificationService')
+@inject(Router, 'BpmnStudioClient', 'NotificationService', EventAggregator)
 export class ConfigPanel {
 
   private router: Router;
   private bpmnStudioClient: BpmnStudioClient;
   private notificationService: NotificationService;
+  private eventAggregator: EventAggregator;
 
   public config: any = environment.bpmnStudioClient;
 
-  constructor(router: Router, bpmnStudioClient: BpmnStudioClient, notificationService: NotificationService) {
+  constructor(router: Router, bpmnStudioClient: BpmnStudioClient, notificationService: NotificationService, eventAggregator: EventAggregator) {
     this.router = router;
     this.bpmnStudioClient = bpmnStudioClient;
     this.config.baseRoute = environment.bpmnStudioClient.baseRoute;
     this.notificationService = notificationService;
+    this.eventAggregator = eventAggregator;
   }
 
   public updateSettings(): void {
@@ -32,6 +35,7 @@ export class ConfigPanel {
     environment.processengine.routes.userTasks =  `${this.config.baseRoute}/datastore/UserTask`;
     this.bpmnStudioClient.updateConfig(this.config);
     this.notificationService.showNotification(NotificationType.SUCCESS, 'Sucessfully saved settings!');
+    this.eventAggregator.publish('statusbar:baseRoute:update', this.config.baseRoute);
     this.router.navigateBack();
   }
 
