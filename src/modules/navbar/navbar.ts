03efa370f8d1c4dc17174f21edc65d113e835a8f
bastian schnittger <bastian.schnittger@5minds.de>
2018-05-15 10:02:01 +0200

    implement event communication with processdefdetail

--- src/modules/navbar/navbar.ts
+++ src/modules/navbar/navbar.ts
@@ -1,14 +1,20 @@
 import {EventAggregator} from 'aurelia-event-aggregator';
 import {bindable, inject} from 'aurelia-framework';
 import {RouteConfig, Router} from 'aurelia-router';
+import { IProcessDefEntity } from '../../contracts';
 
 @inject(Router, EventAggregator)
 export class NavBar {
   private router: Router;
   private eventAggregator: EventAggregator;
+  private exportButton: HTMLButtonElement;
+  private exportSpinner: HTMLElement;
+  private exportIcon: HTMLElement;
 
-  public activeRouteTitle: string;
   @bindable() public showSolutionExplorer: boolean;
+  public activeRouteTitle: string;
+  public showTools: boolean = false;
+  public process: IProcessDefEntity;
 
   constructor(router: Router, eventAggregator: EventAggregator) {
     this.router = router;
@@ -20,6 +26,19 @@ export class NavBar {
     this.eventAggregator.subscribe('router:navigation:complete', () => {
       this.dertermineActiveRoute();
     });
+
+    this.eventAggregator.subscribe('navbar:tools:show', (process: IProcessDefEntity) => {
+      this.showTools = true;
+      this.process = process;
+    });
+
+    this.eventAggregator.subscribe('navbar:tools:hide', () => {
+      this.showTools = false;
+    });
+
+    this.eventAggregator.subscribe('navbar:process:update', (process: IProcessDefEntity) => {
+      this.process = process;
+    });
   }
 
   public navigate(routeTitle: string): void {
@@ -30,9 +49,24 @@ export class NavBar {
     this.router.navigate(`/${route.route}`);
   }
 
+  public navigateBack(): void {
+    this.router.navigateBack();
+  }
+
   public toggleSolutionExplorer(): void {
     this.showSolutionExplorer = !this.showSolutionExplorer;
+  }
 
+  public saveDiagram(): void {
+    this.eventAggregator.publish('processdefdetail:diagram:save');
+  }
+
+  public exportDiagram(exportAs: string): void {
+    this.eventAggregator.publish(`processdefdetail:diagram:exportAs:${exportAs}`);
+  }
+
+  public startProcess(): void {
+    this.eventAggregator.publish('processdefdetail:process:start');
   }
 
   private isRouteActive(routeTitle: string): boolean {
