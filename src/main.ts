23c9fda0913a255bef49d009a96c78ab637bbeca
Christoph Gnip <christoph.gnip@gmail.com>
2018-05-22 14:44:47 +0200

    :lipstick: Improve href Handling and String to Display

--- src/main.ts
+++ src/main.ts
@@ -70,9 +70,14 @@ export function configure(aurelia: Aurelia): void {
         if (status !== 'error') {
           return;
         }
-        const targetHref: string = `javascript:nodeRequire('open')('https://github.com/process-engine/bpmn-studio/issues/316')`;
-        const errorMessage: string = `Failed to start ProcessEngine.
-For further information <a href='${targetHref}'>click here</a>.`;
+        /* This is the URL to an issue in GitHub, describing 
+         * what the user can do about this failure.
+         *
+         * TODO: Implement a proper FAQ section and link to that.
+         */ 
+        const targetHref: string = `<a href="javascript:nodeRequire('open')('https://github.com/process-engine/bpmn-studio/issues/316')">click here</a>`;
+        
+        const errorMessage: string = `Failed to start ProcessEngine. For further information ${targetHref}.`;
         const notificationService: NotificationService = aurelia.container.get('NotificationService');
 
         notificationService.showNonDisappearingNotification(NotificationType.ERROR, errorMessage);
