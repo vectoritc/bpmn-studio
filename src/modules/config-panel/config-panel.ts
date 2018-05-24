68360f4134ad15501ae4d67498666364cdc61030
Alex😎 <alexander.kasten@5minds.de>
2018-05-24 17:03:21 +0200

    :sparkles: Logs the user out, when changing the backend route

--- src/modules/config-panel/config-panel.ts
+++ src/modules/config-panel/config-panel.ts
@@ -4,27 +4,35 @@ import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import environment from '../../environment';
 import {NotificationType} from './../../contracts/index';
+import {AuthenticationService} from './../authentication/authentication.service';
 import {NotificationService} from './../notification/notification.service';
 
-@inject(Router, 'BpmnStudioClient', 'NotificationService', EventAggregator)
+@inject(Router, 'BpmnStudioClient', 'NotificationService', EventAggregator, 'AuthenticationService')
 export class ConfigPanel {
 
   private router: Router;
   private bpmnStudioClient: BpmnStudioClient;
   private notificationService: NotificationService;
   private eventAggregator: EventAggregator;
+  private _authenticationService: AuthenticationService;
 
   public config: any = environment.bpmnStudioClient;
 
-  constructor(router: Router, bpmnStudioClient: BpmnStudioClient, notificationService: NotificationService, eventAggregator: EventAggregator) {
+  constructor(router: Router,
+              bpmnStudioClient: BpmnStudioClient,
+              notificationService: NotificationService,
+              eventAggregator: EventAggregator,
+              authenticationService: AuthenticationService) {
     this.router = router;
     this.bpmnStudioClient = bpmnStudioClient;
     this.config.processEngineRoute = environment.bpmnStudioClient.baseRoute;
     this.notificationService = notificationService;
     this.eventAggregator = eventAggregator;
+    this._authenticationService = authenticationService;
   }
 
   public updateSettings(): void {
+    this._authenticationService.logout();
     environment.bpmnStudioClient.baseRoute = this.config.processEngineRoute;
     window.localStorage.setItem('processEngineRoute', this.config.processEngineRoute);
     environment.processengine.routes.processes = `${this.config.processEngineRoute}/datastore/ProcessDef`;
