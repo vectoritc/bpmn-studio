804c8dd0e04d9cdfbe282d31b43d80731c97165e
Alex😎 <alexander.kasten@5minds.de>
2018-05-25 10:41:01 +0200

    :fire: Remove unused stuff

--- src/modules/user-login/user-login.ts
+++ src/modules/user-login/user-login.ts
@@ -5,36 +5,19 @@ import {IAuthenticationService, IIdentity} from '../../contracts/index';
 export class UserLogin {
 
   private authenticationService: IAuthenticationService;
-  private dropdown: HTMLLIElement;
-  private windowClickListener: (event: MouseEvent) => void;
-  @bindable()
   private username: string;
-  @bindable()
   private password: string;
-  @bindable()
   private loginError: string;
 
   constructor(authenticationService: IAuthenticationService) {
     this.authenticationService = authenticationService;
-    this.windowClickListener = (event: MouseEvent): void => {
-      const node: Node = event.target as Node;
-      if (this.dropdown.contains(node)) {
-        return;
-      }
-      this.closeDropdown();
-    };
   }
 
-  public get hasValidInput(): boolean {
-    const validUsername: boolean = this.username !== null && this.username !== undefined && this.username !== '';
-    const validPassword: boolean = this.password !== null && this.password !== undefined && this.password !== '';
-    return validUsername && validPassword;
   }
 
   public async login(): Promise<void> {
     try {
       await this.authenticationService.login(this.username, this.password);
-      this.closeDropdown();
       this.username = null;
       this.password = null;
       this.loginError = null;
@@ -45,27 +28,6 @@ export class UserLogin {
 
   public logout(): void {
     this.authenticationService.logout();
-    this.closeDropdown();
-  }
-
-  public toggleDropdown(): void {
-    if (this.dropdown.classList.contains('open')) {
-      this.closeDropdown();
-    } else {
-      this.openDropdown();
-    }
-  }
-
-  public openDropdown(): void {
-    this.dropdown.classList.add('open');
-    setTimeout(() => {
-      window.addEventListener('click', this.windowClickListener);
-    });
-  }
-
-  public closeDropdown(): void {
-    this.dropdown.classList.remove('open');
-    window.removeEventListener('click', this.windowClickListener);
   }
 
   @computedFrom('authenticationService.tokenRepository.token')
