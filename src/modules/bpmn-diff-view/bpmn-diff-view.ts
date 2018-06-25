eed98f970e504884e4215d8d5491e10a470ae534
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-06-25 15:41:41 +0200

    :lipstick: Mark Changes in DiffView

--- src/modules/bpmn-diff-view/bpmn-diff-view.ts
+++ src/modules/bpmn-diff-view/bpmn-diff-view.ts
@@ -3,6 +3,10 @@ import { IBpmnModeler } from '../../contracts';
 
 import * as bundle from '@process-engine/bpmn-js-custom-bundle';
 
+import {IModeling,
+  IShape,
+  NotificationType} from '../../contracts/index';
+
 export class BpmnDiffView {
   private _leftViewer: IBpmnModeler;
   private _rightViewer: IBpmnModeler;
@@ -37,6 +41,51 @@ export class BpmnDiffView {
   }
 
   public changesChanged(): void {
+    const addedElements: any = this.changes._added;
+    const deletedElements: any = this.changes._removed;
+    const changedElements: any = this.changes._changed;
+    const layoutChangedElements: any = this.changes._layoutChanged;
+
+    this._markDeletedElements(deletedElements);
+    this._markChangedElements(changedElements);
+    this._markLayoutChangedElements(layoutChangedElements);
+    this._markAddedElements(addedElements);
+  }
+
+  private _markAddedElements(addedElements: any): void {
+    for (const elementId in addedElements) {
+      this._addColorMarker(elementId, this._rightViewer, 'added');
+    }
+  }
+
+  private _markDeletedElements(deletedElemnts: any): void {
+    for (const elementId in deletedElemnts) {
+      this._addColorMarker(elementId, this._leftViewer, 'deleted');
+    }
+  }
+
+  private _markLayoutChangedElements(layoutChangedElements: any): void {
+    for (const elementId in layoutChangedElements) {
+      this._addColorMarker(elementId, this._leftViewer, 'layout-changed');
+      this._addColorMarker(elementId, this._rightViewer, 'layout-changed');
+    }
+  }
+
+  private _markChangedElements(changedElements: any): void {
+    for (const elementId in changedElements) {
+      if (changedElements[elementId].$type === undefined) {
+        continue;
+      }
+
+      this._addColorMarker(elementId, this._leftViewer, 'changed');
+      this._addColorMarker(elementId, this._rightViewer, 'changed');
+    }
+  }
+
+  private _addColorMarker(elementId: string, viewer: IBpmnModeler, markerType: string): void {
+    const canvas: any = viewer.get('canvas');
+
+    canvas.addMarker(elementId, markerType);
   }
 
   public xmlChanged(): void {
