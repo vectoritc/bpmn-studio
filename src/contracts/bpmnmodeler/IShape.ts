918425975670ed9afddea46d6172111804c64494
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-07-03 12:44:47 +0200

    :lipstick: Add attrs to IShape

--- src/contracts/bpmnmodeler/IShape.ts
+++ src/contracts/bpmnmodeler/IShape.ts
@@ -2,6 +2,7 @@ import {IDocumentation} from './bpmnElements/IDocumentation';
 import {IModdleElement} from './index';
 
 export interface IShape {
+  attrs?: object;
   businessObject: IModdleElement;
   id: string;
   type: string;
