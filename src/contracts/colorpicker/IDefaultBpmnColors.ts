8e88b1e649441a94be5aa8c35659956056bb80f3
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-07-03 12:49:43 +0200

    :sparkles: Move DefaultBpmnColors to Contracts

---
+++ src/contracts/colorpicker/IDefaultBpmnColors.ts
@@ -0,0 +1,10 @@
+import {IColorPickerColor} from './index';
+
+export interface IDefaultBpmnColors {
+  red: IColorPickerColor;
+  blue: IColorPickerColor;
+  green: IColorPickerColor;
+  purple: IColorPickerColor;
+  orange: IColorPickerColor;
+  none: IColorPickerColor;
+}
