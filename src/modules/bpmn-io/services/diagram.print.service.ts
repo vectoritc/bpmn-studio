5d4bf1d56c5ce6c4860af677b3e0e6607837191d
robpal94 <robin.palkovits@5minds.de>
2018-07-04 15:53:13 +0200

    Update generateImage method

--- src/modules/bpmn-io/services/diagram.print.service.ts
+++ src/modules/bpmn-io/services/diagram.print.service.ts
@@ -1,10 +1,7 @@
 import {IBpmnModeler, IProcessDefEntity} from '../../../contracts';
-import {ICanvgOptions} from '../../../contracts';
 import {IPrintService} from '../../../contracts/printing/IPrintService';
 
-import * as canvg from 'canvg-browser';
 import * as print from 'print-js';
-
 export class DiagramPrintService implements IPrintService {
 
   private _modeler: IBpmnModeler;
@@ -40,8 +37,7 @@ export class DiagramPrintService implements IPrintService {
       });
     });
   }
-
-  private _generateImageFromSVG(desiredImageType: string, svg: any): string {
+  private async _generateImageFromSVG(desiredImageType: string, svg: string): Promise<string> {
     const encoding: string = `image/${desiredImageType}`;
     const canvas: HTMLCanvasElement = document.createElement('canvas');
     const context: CanvasRenderingContext2D = canvas.getContext('2d');
@@ -63,25 +59,17 @@ export class DiagramPrintService implements IPrintService {
     canvas.width = svgWidth * pixelRatio;
     canvas.height = svgHeight * pixelRatio;
 
-    const canvgOptions: ICanvgOptions = {
-      ignoreDimensions: true,
-      scaleWidth: canvas.width,
-      scaleHeight: canvas.height,
-    };
-
-    canvg(canvas, svg, canvgOptions);
-
-    // make the background white for every format
-    context.globalCompositeOperation = 'destination-over';
+    // Make the background white for every format
     context.fillStyle = 'white';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    // get image as base64 datastring
-    const image: string = canvas.toDataURL(encoding);
-    return image;
+    // Draw the image to the canvas
+    const imageDataURL: string = await this._drawSVGToCanvas(svg, canvas, context, encoding);
+
+    return imageDataURL;
   }
 
-      /**
+  /**
    * Calculate the pixel ratio for the given DPI.
    * The Pixel Ratio is the factor which is needed, to extend the
    * the width and height of a canvas to match a rendered resolution
@@ -106,4 +94,58 @@ export class DiagramPrintService implements IPrintService {
     return pixelRatio;
   }
 
+  /**
+   * Draws a given SVG image to a Canvas and converts it to an image.
+   *
+   * @param svgContent SVG Content that should be drawn to the image.
+   * @param canvas Canvas, in which the SVG image should be drawn.
+   * @param context Context of the Canvas.
+   * @param encoding Encoding of the output image.
+   * @returns The URL which points to the rendered image.
+   */
+  private async _drawSVGToCanvas(
+    svgContent: string,
+    canvas: HTMLCanvasElement,
+    context: CanvasRenderingContext2D,
+    encoding: string): Promise<string> {
+
+    const imageElement: HTMLImageElement = document.createElement('img');
+
+   /*
+    * This makes sure, that the base64 encoded SVG does not contain any
+    * escaped html characters (such as &lt; instead of <).
+    *
+    * TODO: The unescape Method is marked as deprecated.
+    * See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/unescape
+    *
+    * The problem is, that the replacement method decodeURI does not work in this case
+    * (it behaves kinda different in some situations).
+    * Event the MDN use the unescape method to solve this kind of problem:
+    * https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/btoa#Unicode_strings
+    *
+    * There is an npm packet that implements the original unescape function.
+    * Maybe we can use this to make sure that this won't cause any
+    * problems in the future.
+    */
+    const encodedSVG: string = btoa(unescape(encodeURIComponent(svgContent)));
+    imageElement.setAttribute('src', `data:image/svg+xml;base64, ${encodedSVG}`);
+
+    const loadImagePromise: Promise<string> = new Promise((resolve: Function, reject: Function): void => {
+      imageElement.onload = (): void => {
+        context.drawImage(imageElement, 0, 0, canvas.width, canvas.height);
+        const encodedImageURL: string = canvas.toDataURL(encoding);
+        resolve(encodedImageURL);
+      };
+
+      imageElement.onerror = (errorEvent: ErrorEvent): void => {
+       /*
+        * TODO: Find out if we can reject the promise with a more specify
+        * error here.
+        */
+        reject(errorEvent);
+      };
+    });
+
+    return loadImagePromise;
+  }
 }
