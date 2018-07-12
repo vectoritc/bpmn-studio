f0e9d493a4d406d67401bf7748643d3fdca4687a
robpal94 <robin.palkovits@5minds.de>
2018-07-12 11:44:35 +0200

    :fire: remove the old export service

--- src/contracts/export/IDiagramExportService.ts
+++
@@ -1,8 +0,0 @@
-export interface IDiagramExportService {
-  exportBPMN(xml?: string): Promise<string>;
-  exportSVG(svg?: string): Promise<string>;
-  exportPNG(svg?: string): Promise<string>;
-  exportJPEG(svg?: string): Promise<string>;
-  updateXML(newXML: string): void;
-  updateSVG(newSVG: string): void;
-}
--- src/contracts/export/index.ts
+++
@@ -1,1 +0,0 @@
-export {IDiagramExportService} from './IDiagramExportService';
