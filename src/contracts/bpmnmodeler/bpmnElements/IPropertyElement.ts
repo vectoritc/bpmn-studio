cd960ef8442ada2cb51110c6d1e6bba6df6dad3a
Alex😎 <alexander.kasten@5minds.de>
2018-07-17 09:44:05 +0200

    :truck: Rename IPropertyElement to IPropertiesElement

--- src/contracts/bpmnmodeler/bpmnElements/IPropertyElement.ts
+++
@@ -1,6 +0,0 @@
-import {IModdleElement} from './IModdleElement';
-import {IProperty} from './IProperty';
-
-export interface IPropertyElement extends IModdleElement {
-  values?: Array<IProperty>;
-}
