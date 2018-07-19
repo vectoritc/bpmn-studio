d843596c7ef5d14e0ffbb516dc96f3b99678d597
Paul <paul.heidenreich@5minds.de>
2018-07-19 16:52:56 +0200

    :memo: Add Documentation to DiagramValidationService

--- src/modules/diagram-validation-service/DiagramValidationService.ts
+++ src/modules/diagram-validation-service/DiagramValidationService.ts
@@ -5,6 +5,23 @@ import {
 } from '../../contracts';
 import {DiagramValidator} from './DiagramValidator';
 
+/**
+ * This class provides a fluent API to validate BPMN diagrams.
+ *
+ * In this class the rules for valid diagrams are defined. Each
+ * validation, for example `isBPMN()` is backed by a IValidationRuleSet.
+ *
+ * A IValidationRuleSet contains one or more IValidationRules and an
+ * error message. If one more more rules of the ruleset fail on the
+ * input, an error is thrown.
+ *
+ * To start a validation call `validate(XML)` with the suspect XML string.
+ * Then call the rules you would like to validate, for example `isBPMN()`.
+ * After adding all rules, call `throwIfError()`, this will either pass or
+ * throw a descriptive error.
+ *
+ * RuleSets are defined in the `_rules` field of this class.
+ */
 export class DiagramValidationService implements IDiagramValidationService {
 
   private _rules: IDiagramValidationRules = {
