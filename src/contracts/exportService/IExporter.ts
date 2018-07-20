9819ba60d2dbd428cf2e4289976238bb2e33aea7
robpal94 <robin.palkovits@5minds.de>
2018-07-20 13:31:57 +0200

    :fire: remove old contracts

--- src/contracts/exportService/IExporter.ts
+++
@@ -1,9 +0,0 @@
-export interface IExporter {
-  /**
-   * Exports the current diagram as the last defined format.
-   *
-   * @params filename Name of the file that will be exported.
-   * @returns the exported diagram.
-   */
-  export(filename: string): Promise<void>;
-}
