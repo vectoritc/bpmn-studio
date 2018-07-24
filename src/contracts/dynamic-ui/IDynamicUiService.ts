631a24b6c019c4b03287e1b075741a180c009f9b
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-07-24 15:38:01 +0200

    :lipstick: Adjust Interface of DynamicUiService to Management API

--- src/contracts/dynamic-ui/IDynamicUiService.ts
+++ src/contracts/dynamic-ui/IDynamicUiService.ts
@@ -1,6 +1,13 @@
-import {UserTask, UserTaskResult} from '@process-engine/management_api_contracts';
+import {ManagementContext, UserTask, UserTaskResult} from '@process-engine/management_api_contracts';
 
 export interface IDynamicUiService {
-  sendProceedAction(processModelKey: string,  correlationId: string, userTaskId: string, userTaskResult: UserTaskResult): void;
-  getUserTaskById(userTaskId: string, correlationId: string): Promise<UserTask>;
+  finishUserTask(managementContext: ManagementContext,
+                 processModelKey: string,
+                 correlationId: string,
+                 userTaskId: string,
+                 userTaskResult: UserTaskResult): void;
+
+  getUserTaskFromCorrelationById(managementContext: ManagementContext,
+                                 userTaskId: string,
+                                 correlationId: string): Promise<UserTask>;
 }
