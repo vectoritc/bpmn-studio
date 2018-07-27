e452ab7e5c61dff8ad9a2a70873a801627acb798
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-07-27 15:52:31 +0200

    :lipstick: Import from Management API Contracts Instead Of Consumer API Contracts

--- src/contracts/form-fields/IBooleanFormField.ts
+++ src/contracts/form-fields/IBooleanFormField.ts
@@ -1,4 +1,4 @@
-import {UserTaskFormField} from '@process-engine/consumer_api_contracts';
+import {UserTaskFormField} from '@process-engine/management_api_contracts';
 
 export interface IBooleanFormField extends UserTaskFormField {
   value: boolean;
--- src/contracts/form-fields/IEnumFormField.ts
+++ src/contracts/form-fields/IEnumFormField.ts
@@ -1,4 +1,4 @@
-import {UserTaskFormField, UserTaskFormFieldType} from '@process-engine/consumer_api_contracts';
+import {UserTaskFormField, UserTaskFormFieldType} from '@process-engine/management_api_contracts';
 
 export interface IEnumFormField extends UserTaskFormField {
   value: string;
