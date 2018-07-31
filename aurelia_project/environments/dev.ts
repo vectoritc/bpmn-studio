2de169880d6a7d262c2722ef98a82a342b7bed33
Paul <paul.heidenreich@5minds.de>
2018-07-31 11:23:17 +0200

    :bug: Only Show Diagram Upload Button on Diagram Detail View

--- aurelia_project/environments/dev.ts
+++ aurelia_project/environments/dev.ts
@@ -46,6 +46,8 @@ export default {
       updateProcess: 'navbar:process:update',
       disableSaveButton: 'navbar:saveButton:disable',
       enableSaveButton: 'navbar:saveButton:enable',
+      showDiagramUploadButton: 'navbar:diagramUploadButton:show',
+      hideDiagramUploadButton: 'navbar:diagramUploadButton:hide',
     },
     processDefDetail: {
       printDiagram: 'processdefdetail:diagram:print',
