25bcccb3b83d54b1d86fc5258cf3428cdd522a6e
Christoph Gnip <christoph.gnip@gmail.com>
2018-08-03 10:36:25 +0200

    :recycle: Rework the Script; Add Version

--- build_clean_setup.bash
+++ build_clean_setup.bash
@@ -1,35 +1,37 @@
 #!/bin/bash
 
+_VERSION="0.0.1"
+
 ############
 # This Script builds a clean setup of the BPMN - Studio.
 # Keep in mind that your npm cache will be cleaned.
 ############
 
 # Remove the node modules folder
-echo Removing Node Modules....
+echo "Removing Node Modules...."
 rm -rf node_modules/
 
 # Remove the package-lock file if it exists.
-if [ -e "package-lock.json" ]; then
-  echo Removing package lock file...
+if [[ -e "package-lock.json" ]]; then
+  echo "Removing package lock file..."
   rm package-lock.json
 fi
 
 # Clear the npm cache
-echo Clearing npm cache...
+echo "Clearing npm cache..."
 npm cache clean --force
 
 # Install Node Modules
-echo Installing node modules...
+echo "Installing node modules..."
 npm install --no-package-lock
 
 # If npm install failed, we can abort the script execution since
 # the build process would fail anyways.
-if [[ "$?" -ne "0" ]]; then
-  echo NPM Install Failed. Exiting...
+if [[ $? -ne 0 ]]; then
+  echo "NPM Install Failed. Exiting..."
   exit 1;
 fi
 
 # Build
-echo Building...
+echo "Building..."
 npm run build
