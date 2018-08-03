86061705988d07389bfcb78171b71cd18a2fb1f9
Alex😎 <alexander.kasten@5minds.de>
2018-08-03 17:41:38 +0200

    :fire: Remove npm i and npm build from reset script

--- dev_tooling/scripts/build_clean_setup.bash
+++ dev_tooling/scripts/build_clean_setup.bash
@@ -20,18 +20,3 @@ fi
 # Clear the npm cache
 echo "Clearing npm cache..."
 npm cache clean --force
-
-# Install Node Modules
-echo "Installing node modules..."
-npm install --no-package-lock
-
-# If npm install failed, we can abort the script execution since
-# the build process would fail anyways.
-if [[ $? -ne 0 ]]; then
-  echo "NPM Install Failed. Exiting..."
-  exit 1;
-fi
-
-# Build
-echo "Building..."
-npm run build
