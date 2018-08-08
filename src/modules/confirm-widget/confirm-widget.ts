c6d3f23a3217312566f9305b6406348d586b56ff
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-08-08 16:27:27 +0200

    :sparkles: Render Form Fields in Confirm Widget

--- src/modules/confirm-widget/confirm-widget.ts
+++ src/modules/confirm-widget/confirm-widget.ts
@@ -1,11 +1,21 @@
 import {UserTaskConfig, UserTaskFormField, UserTaskFormFieldType} from '@process-engine/management_api_contracts';
-import {bindable} from 'aurelia-framework';
+import {bindable, inject} from 'aurelia-framework';
+import {NotificationType} from '../../contracts/index';
+import {NotificationService} from '../notification/notification.service';
 
+@inject('NotificationService')
 export class ConfirmWidget {
 
   @bindable()
   public userTaskConfig: UserTaskConfig;
+  public formFields: Array<UserTaskFormField>;
   public confirmMessage: string;
+
+  private _notificationService: NotificationService;
+
+  constructor(notificationService: NotificationService) {
+    this._notificationService = notificationService;
+  }
 
   public attached(): void {
     const booleanFormField: UserTaskFormField = this._getBooleanFormField();
@@ -14,7 +24,30 @@ export class ConfirmWidget {
     if (hasUserTaskBooleanFormField) {
       this.confirmMessage = booleanFormField.label;
     }
+
+    this.formFields = this.userTaskConfig.formFields.slice(1);
   }
+
+  public getFieldControl(field: UserTaskFormField): string {
+    switch (field.type) {
+      case UserTaskFormFieldType.enum:
+        return 'dropdown';
+      case UserTaskFormFieldType.string:
+        return 'textbox';
+      case UserTaskFormFieldType.boolean:
+        return 'checkbox';
+      case UserTaskFormFieldType.long:
+        return 'number';
+      default:
+        const notSupportedType: string = field.type !== undefined ? field.type : 'Custom Type';
+        const errorMessage: string = `Not supported form field type: ${notSupportedType}.`
+                                   + `</br>Please change the form field type with id "${field.id}".`;
+
+        this._notificationService.showNotification(NotificationType.ERROR, errorMessage);
+        return null;
+    }
+  }
+
   private _getBooleanFormField(): UserTaskFormField {
     const formFields: Array<UserTaskFormField> = this.userTaskConfig.formFields;
 
