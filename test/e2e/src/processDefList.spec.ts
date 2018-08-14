a0df556bafbe75f12ba66a509cbaaa979f3f97d4
dominikkaminski <dominik.kaminski@5minds.de>
2018-08-14 11:52:56 +0200

    =:recycle: Revert changes of processDefList.spec.ts

--- test/e2e/src/processDefList.spec.ts
+++ test/e2e/src/processDefList.spec.ts
@@ -5,7 +5,7 @@ import {ProcessDefListPage} from './pages/processdef-list-page';
 describe('processDefList', () => {
 
   const defaultTimeoutMS: number = 2000;
-  const aureliaUrl: string = 'http://127.0.0.1:9000';
+  const aureliaUrl: string = 'http://localhost:9000';
 
   browser.driver.manage().deleteAllCookies();
   browser.get(aureliaUrl);
