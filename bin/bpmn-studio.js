2e298f53115b98472edc20647eea62ce684af42b
dominikkaminski <dominik.kaminski@5minds.de>
2018-08-16 09:14:28 +0200

    :art: Refactor error message in _applicationHostIsValid function

--- bin/bpmn-studio.js
+++ bin/bpmn-studio.js
@@ -59,7 +59,7 @@ function _applicationHostIsValid(host) {
   const host_array = host.split('.');
   const addressHasNotFourOctetts = host_array.length !== 4;
   if (addressHasNotFourOctetts) {
-    console.log(host + " is not a valid ip address [0.0.0.0].\n");
+    console.log("The specified host: '"+ host +"' is not a valid IP address, like: '0.0.0.0'.\n");
     return false;
   }
   return true;
