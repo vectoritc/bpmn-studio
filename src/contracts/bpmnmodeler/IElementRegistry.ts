655efc250f86ffedfa93918411990dfd354579ef
Christoph Gnip <christoph.gnip@gmail.com>
2018-08-20 11:12:49 +0200

    :lipstick: Correct the Spelling in Documentation

--- src/contracts/bpmnmodeler/IElementRegistry.ts
+++ src/contracts/bpmnmodeler/IElementRegistry.ts
@@ -3,11 +3,11 @@ import {IShape} from './IShape';
 export interface IElementRegistry {
 
   /**
-   * Register a pair of (element, gfx, (secondaryGFX)).
+   * Register a tuple of (element, gfx, (secondaryGFX)).
    *
    * @param element element
    * @param gfx gfx
-   * @param secondaryGfx optional other element to register, too
+   * @param secondaryGfx Optional, other element to register, too.
    */
   add(element: IShape, gfx: SVGElement, secondaryGfx?: SVGElement): void;
 
@@ -19,30 +19,28 @@ export interface IElementRegistry {
   remove(element: IShape): void;
 
   /**
-   * Update the id of an element.
+   * Update the ID of an element.
    *
-   * @param element Element
+   * @param element Element to update.
    * @param newId New ID of the element
    */
   updateId(element: IShape, newId: string): void;
 
   /**
-   * Return the model element for a given id or graphics.
-   *
-   * @example
+   * Return the model element for a given ID or graphics.
    *
    * elementRegistry.get('SomeElementId_1');
    * elementRegistry.get(gfx);
    *
-   * @param filter Filter for selecting the element
+   * @param filter Filter for selecting the element.
    */
   get(filter: string | SVGElement): IShape;
 
   /**
    * Returns all elements that match a given filter function.
    *
-   * @param filterMethod Filter function
-   * @return The Elements that matches the fiter function.
+   * @param filterMethod Filter function to apply.
+   * @return The Elements that matches the filter function.
    */
   filter(filterMethod: (element: IShape) => Boolean): Array<IShape>;
 
@@ -61,7 +59,7 @@ export interface IElementRegistry {
   forEach(fn: Function): void;
 
   /**
-   * Return the graphical representation of an element or its id.
+   * Return the graphical representation of an element or its ID.
    *
    * @example
    * elementRegistry.getGraphics('SomeElementId_1');
@@ -70,19 +68,19 @@ export interface IElementRegistry {
    * elementRegistry.getGraphics(rootElement, true); // <svg ...>
    *
    *
-   * @param filter ID or definition of the Element.
-   * @param secondary whether to return the secondary connected element
+   * @param filter ID or definition of the element.
+   * @param secondary Whether to return the secondary connected element, or not.
    *
    * @return {SVGElement}
    */
  getGraphics(filter: string | IShape, secondary?: boolean): SVGElement;
 
   /**
-   * Validate the suitability of the given id and signals a problem
-   * with an exception.
+   * Validate the suitability of the given ID; throws an exception 
+   * if there is a problem with the ID.
    *
    * @param id ID of the Element, that should be validated.
-   * @throws Error if the id is empty or already assigned.
+   * @throws Error if the ID is empty or already assigned.
    */
  _validateId(id: string): void;
 
