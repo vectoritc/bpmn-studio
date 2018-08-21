dfe531dfb19b9cd9dc18f5afa63c9c224f5e3333
Alex😎 <alexander.kasten@5minds.de>
2018-08-21 16:08:56 +0200

    :sparkles: Use HeatmapService in Heatmap Component and Display Viewer

--- src/modules/heatmap/heatmap.ts
+++ src/modules/heatmap/heatmap.ts
@@ -1,45 +1,72 @@
 import {inject} from 'aurelia-framework';
 
-import {ManagementApiClientService} from '@process-engine/management_api_client';
-import {ManagementContext, ProcessModelExecution} from '@process-engine/management_api_contracts';
+import {IBpmnModeler, IElementRegistry, IShape} from '../../contracts';
+import {IFlowNodeAssociation, IHeatmapService} from './contracts';
 
-import {AuthenticationService} from '../authentication/authentication.service';
+import * as bundle from '@process-engine/bpmn-js-custom-bundle';
+import {FlowNodeRuntimeInformation} from '@process-engine/kpi_api_contracts';
+import {ProcessModelExecution} from '@process-engine/management_api_contracts';
 
 interface RouteParameters {
   processModelId: string;
 }
 
-@inject('ManagementApiClientService', 'AuthenticationService')
+@inject('HeatmapService')
 export class Heatmap {
-
-  public processModel: ProcessModelExecution.ProcessModel;
+  public viewerContainer: HTMLDivElement;
 
   private _processModelId: string;
-  private _managementApiClient: ManagementApiClientService;
-  private _authenticationService: AuthenticationService;
+  private _heatmapService: IHeatmapService;
+  private _modeler: IBpmnModeler;
+  private _viewer: IBpmnModeler;
 
-  constructor(managementClient: ManagementApiClientService, authenticationService: AuthenticationService) {
-    this._managementApiClient = managementClient;
-    this._authenticationService = authenticationService;
+  constructor(heatmapService: IHeatmapService) {
+    this._heatmapService = heatmapService;
+    this._modeler = new bundle.modeler({
+      moddleExtensions: {
+        camunda: bundle.camundaModdleDescriptor,
+      },
+    });
   }
 
   public async activate(routeParameters: RouteParameters): Promise<void> {
     this._processModelId = routeParameters.processModelId;
-    await this._getProcess();
+    const processModel: ProcessModelExecution.ProcessModel = await this._heatmapService.getProcess(this._processModelId);
+    await this._importXML(processModel.xml, this._modeler);
+
+    const elementRegistry: IElementRegistry  = this._modeler.get('elementRegistry');
+    const associations: Array<IFlowNodeAssociation> = await this._heatmapService.getFlowNodeAssociations(elementRegistry);
+    const flowNodeRuntimeInformation: Array<FlowNodeRuntimeInformation> = await this
+      ._heatmapService
+      .getRuntimeInformationForProcessModel(this._processModelId);
+
+    const xml: string = await this._heatmapService.getColoredXML(associations, flowNodeRuntimeInformation, this._modeler);
+
+    this._viewer = new bundle.viewer({
+      additionalModules:
+      [
+        bundle.ZoomScrollModule,
+        bundle.MoveCanvasModule,
+      ],
+    });
+
+    this._importXML(xml, this._viewer);
   }
 
-  private async _getProcess(): Promise<void> {
-    const context: ManagementContext = this._getManagementContext();
-
-    this.processModel = await this._managementApiClient.getProcessModelById(context, this._processModelId);
+  public attached(): void {
+    this._viewer.attachTo(this.viewerContainer);
   }
 
-  private _getManagementContext(): ManagementContext {
-    const accessToken: string = this._authenticationService.getAccessToken();
-    const context: ManagementContext = {
-      identity: accessToken,
-    };
-
-    return context;
+  private _importXML(xml: string, modeler: IBpmnModeler): Promise<void> {
+    return new Promise((resolve: Function, reject: Function): void => {
+      modeler.importXML(xml, (importXmlError: Error) => {
+        if (importXmlError) {
+          reject(importXmlError);
+          return;
+        }
+        resolve();
+      });
+    });
   }
+
 }
