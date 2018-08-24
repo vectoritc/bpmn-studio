cacd5672ea5ef9eb3a483fcb567844da27cd4058
Dominik Kaminski <dominik.kaminski@5minds.de>
2018-08-24 11:28:26 +0200

    Delete integration-test.sh

--- integration-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export CB_USER="developers@5minds.de"
-export CB_KEY="u20460424e777176"
-export aureliaUrl="http://89.163.137.186:9000"
-export processEngineUrl="http://89.163.137.186:8000" 
-
-# Run this command to apply environment variables: source integration-test.sh
