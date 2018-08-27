934b469b37babb024a6bd4477974004c8a89031a
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-08-27 16:41:21 +0200

    :sparkles: Add Token And Log Data

--- src/modules/inspect-correlation/inspect-correlation.ts
+++ src/modules/inspect-correlation/inspect-correlation.ts
@@ -1,8 +1,14 @@
 import {inject} from 'aurelia-framework';
 
 import * as bundle from '@process-engine/bpmn-js-custom-bundle';
-import {Correlation, IManagementApiService, ManagementContext, ProcessModelExecution} from '@process-engine/management_api_contracts';
+import {
+  Correlation,
+  IManagementApiService,
+  ManagementContext,
+  ProcessModelExecution,
+} from '@process-engine/management_api_contracts';
 
+import {ProcessModel} from '@process-engine/consumer_api_contracts';
 import {IAuthenticationService, IBpmnModeler, NotificationType} from '../../contracts/index';
 import {NotificationService} from '../notification/notification.service';
 import {IInspectCorrelationService} from './contracts';
@@ -14,7 +20,10 @@ interface RouteParameters {
 @inject('InspectCorrelationService', 'NotificationService', 'ManagementApiClientService', 'AuthenticationService')
 export class InspectCorrelation {
   public processModelCorrelations: Array<Correlation>;
+  public correlationSelected: boolean;
   public canvasModel: HTMLElement;
+  public currentToken: string;
+  public currentLog: string;
 
   private _inspectCorrelationService: IInspectCorrelationService;
   private _notificationService: NotificationService;
@@ -51,13 +60,39 @@ export class InspectCorrelation {
     this._diagramViewer.attachTo(this.canvasModel);
   }
 
+  public detached(): void {
+    this.correlationSelected = false;
+  }
+
   public async selectCorrelation(processModelId: string): Promise<void> {
     const managementContext: ManagementContext = this._getManagementContext();
+
+    this.correlationSelected = true;
 
     const processModel: ProcessModelExecution.ProcessModel = await this._managementApiService.getProcessModelById(managementContext, processModelId);
     const xml: string = processModel.xml;
 
     this._importXml(xml);
+    const lorem: string = `Lorem ipsum dolor sit amet, consetetur sadipscing
+    elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+    aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+    dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+    est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+    dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+    et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+    takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+    amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+    ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+    accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+    takimata sanctus est Lorem ipsum dolor sit amet. Duis autem vel eum iriure
+    dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
+    dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio
+    dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te
+    feugait nulla facilisi. Lorem ipsum dolor sit amet,`;
+
+    this.currentToken = `Token: ${lorem}`;
+    this.currentLog = `Log ${lorem}`;
   }
 
   private async _importXml(xml: string): Promise <void> {
