e8c4132b98a168cf982b49eabcc3330d0a1809b6
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-08-30 16:59:01 +0200

    :lipstick: Improve Name of Variable

--- src/modules/inspect-correlation/components/process-instance-list/process-instance-list.ts
+++ src/modules/inspect-correlation/components/process-instance-list/process-instance-list.ts
@@ -3,10 +3,10 @@ import {bindable, bindingMode} from 'aurelia-framework';
 import {Correlation} from '@process-engine/management_api_contracts';
 
 export class ProcessInstanceList {
-  @bindable() public correlation: Correlation;
+  @bindable() public selectedCorrelation: Correlation;
   @bindable({defaultBindingMode: bindingMode.oneWay}) public processModelCorrelations: Array<Correlation>;
 
   public selectCorrelation(selectedCorrelation: Correlation): void {
-    this.correlation = selectedCorrelation;
+    this.selectedCorrelation = selectedCorrelation;
   }
 }
