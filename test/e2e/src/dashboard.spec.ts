5d76929f4664d9921df08d5546f4ed745a824194
dominikkaminski <dominik.kaminski@5minds.de>
2018-08-30 09:15:38 +0200

    Address requested changes of @ElRaptorus

--- test/e2e/src/dashboard.spec.ts
+++ test/e2e/src/dashboard.spec.ts
@@ -1,4 +1,4 @@
-import {browser, protractor, ProtractorExpectedConditions} from 'protractor';
+import {browser, ElementFinder, protractor, ProtractorExpectedConditions} from 'protractor';
 
 import {Dashboard} from './pages/dashboard';
 import {General} from './pages/general';
@@ -31,10 +31,13 @@ describe('Dashboard view', () => {
   });
 
   beforeEach(() => {
-    browser.get(aureliaUrl + dashboard.dashboardLink);
+    const dashboardLink: string = dashboard.dashboardLink;
+    const routerViewContainer: ElementFinder = general.getRouterViewContainer;
+
+    browser.get(aureliaUrl + dashboardLink);
     browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(general.getRouterViewContainer), defaultTimeoutMS);
-      return general.getRouterViewContainer;
+      browser.wait(expectedConditions.visibilityOf(routerViewContainer), defaultTimeoutMS);
+      return routerViewContainer;
     });
   });
 
@@ -45,67 +48,110 @@ describe('Dashboard view', () => {
   // process list section
 
   it('should contain process list.', () => {
-    dashboard.processListTag.isDisplayed().then((dashboardProcessListTagIsDisplayed: boolean) => {
+    const processListTag: ElementFinder = dashboard.processListTag;
+
+    processListTag
+    .isDisplayed()
+    .then((dashboardProcessListTagIsDisplayed: boolean) => {
       expect(dashboardProcessListTagIsDisplayed).toBeTruthy();
     });
   });
 
   it('should contain at least process definition in running section.', () => {
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(dashboard.firstProcessRunningListItems), defaultTimeoutMS);
-      return dashboard.firstProcessRunningListItems;
+    const firstProcessRunningListItems: ElementFinder = dashboard.firstProcessRunningListItems;
+
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(firstProcessRunningListItems), defaultTimeoutMS);
+      return firstProcessRunningListItems;
     });
 
-    dashboard.countOfProcessRunningListItems().then((countOfProcessRunningListItems: number) => {
+    dashboard.countOfProcessRunningListItems()
+    .then((countOfProcessRunningListItems: number) => {
       expect(countOfProcessRunningListItems).toBeGreaterThanOrEqual(1);
     });
   });
 
   it('should contain recently started process in running section.', () => {
     const correlationId: string = processModel.getCorrelationId();
+    const firstProcessRunningListItemsById: ElementFinder = dashboard.firstProcessRunningListItemsById(correlationId);
 
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(dashboard.firstProcessRunningListItemsById(correlationId)), defaultTimeoutMS);
-      return dashboard.firstProcessRunningListItemsById(correlationId);
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(firstProcessRunningListItemsById), defaultTimeoutMS);
+      return firstProcessRunningListItemsById;
     });
 
-    dashboard.countOfProcessRunningListItemsByCorrelationId(correlationId).then((countOfProcessRunningListItems: number) => {
+    const countOfProcessRunningListItemsByCorrelationId: Promise<number> = dashboard.countOfProcessRunningListItemsByCorrelationId(correlationId);
+
+    countOfProcessRunningListItemsByCorrelationId
+    .then((countOfProcessRunningListItems: number) => {
       expect(countOfProcessRunningListItems).toBe(1);
     });
   });
 
   it('should be possible to open process model by click on hyperlink in table.', () => {
     const correlationId: string = processModel.getCorrelationId();
+    const hyperlinkOfProcessRunningListItemByCorrelationId: ElementFinder = dashboard.hyperlinkOfProcessRunningListItemByCorrelationId(correlationId);
 
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(
-        dashboard.hyperlinkOfProcessRunningListItemByCorrelationId(correlationId)), defaultTimeoutMS);
-      return dashboard.hyperlinkOfProcessRunningListItemByCorrelationId(correlationId);
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(
+        hyperlinkOfProcessRunningListItemByCorrelationId), defaultTimeoutMS);
+      return hyperlinkOfProcessRunningListItemByCorrelationId;
     });
-    dashboard.openProcessModelByClickOnModelIdInProcessRunningList(correlationId).then(() => {
-      browser.getCurrentUrl().then((currentBrowserUrl: string) => {
-        expect(currentBrowserUrl).toContain(processModel.processModelUrl(correlationId));
+
+    const openProcessModelByClickOnModelIdInProcessRunningList: any = dashboard.openProcessModelByClickOnModelIdInProcessRunningList(correlationId);
+    const processModelUrl: string = processModel.processModelUrl(processModelId);
+
+    openProcessModelByClickOnModelIdInProcessRunningList
+    .then(() => {
+      browser
+      .getCurrentUrl()
+      .then((currentBrowserUrl: string) => {
+        expect(currentBrowserUrl).toContain(processModelUrl);
       });
     });
   });
 
   it('should be possible to open user tasks by click on hyperlink in table.', () => {
     const correlationId: string = processModel.getCorrelationId();
+    const hyperlinkOfUserTasksInProcessRunningListItemByCorrelationId: ElementFinder
+            = dashboard.hyperlinkOfUserTasksInProcessRunningListItemByCorrelationId(correlationId);
 
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(
-        dashboard.hyperlinkOfUserTasksInProcessRunningListItemByCorrelationId(correlationId)), defaultTimeoutMS);
-      return dashboard.hyperlinkOfUserTasksInProcessRunningListItemByCorrelationId(correlationId);
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(hyperlinkOfUserTasksInProcessRunningListItemByCorrelationId), defaultTimeoutMS);
+      return hyperlinkOfUserTasksInProcessRunningListItemByCorrelationId;
     });
-    dashboard.openUserTasksByClickOnModelIdInProcessRunningList(correlationId).then(() => {
-      browser.getCurrentUrl().then((currentBrowserUrl: string) => {
-        expect(currentBrowserUrl).toContain(processModel.userTasksUrl(processModelId));
+
+    const openUserTasksByClickOnModelIdInProcessRunningList: any = dashboard.openUserTasksByClickOnModelIdInProcessRunningList(correlationId);
+    const userTasksUrl: string = processModel.userTasksUrl(processModelId);
+
+    openUserTasksByClickOnModelIdInProcessRunningList
+    .then(() => {
+      browser
+      .getCurrentUrl()
+      .then((currentBrowserUrl: string) => {
+        expect(currentBrowserUrl).toContain(userTasksUrl);
       });
     });
   });
 
   it('should contain task list.', () => {
-    dashboard.taskListContainer.isDisplayed().then((dashboardTaskListContainer: boolean) => {
+    const taskListContainer: ElementFinder = dashboard.taskListContainer;
+
+    taskListContainer
+    .isDisplayed()
+    .then((dashboardTaskListContainer: boolean) => {
       expect(dashboardTaskListContainer).toBeTruthy();
     });
   });
@@ -113,80 +159,138 @@ describe('Dashboard view', () => {
   // task list section
 
   it('should contain at least task definition in tasks waiting section.', () => {
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(dashboard.firstTaskListItems), taskListTimeoutMS);
-      return dashboard.firstTaskListItems;
+    const firstTaskListItems: ElementFinder = dashboard.firstTaskListItems;
+
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(firstTaskListItems), taskListTimeoutMS);
+      return firstTaskListItems;
     });
 
-    dashboard.countOfTasksWaitingListItems().then((countOfTasksWaitingListItems: number) => {
+    const countOfTasksWaitingListItem: Promise<number> = dashboard.countOfTasksWaitingListItems();
+
+    countOfTasksWaitingListItem
+    .then((countOfTasksWaitingListItems: number) => {
       expect(countOfTasksWaitingListItems).toBeGreaterThanOrEqual(1);
     });
   });
 
   it('should contain recently started task in tasks waiting section.', () => {
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(dashboard.firstTaskWaitingById(processModelId)), taskListTimeoutMS);
-      return dashboard.firstTaskWaitingById(processModelId);
+    const firstTaskWaitingById: ElementFinder = dashboard.firstTaskWaitingById(processModelId);
+
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(firstTaskWaitingById), taskListTimeoutMS);
+      return firstTaskWaitingById;
     });
 
-    dashboard.countOfTasksWaitingListItemsById(processModelId).then((countOfTasksWaitingListItemsById: number) => {
-      expect(countOfTasksWaitingListItemsById).toBe(1);
+    const countOfTasksWaitingListItemsById: Promise<number> = dashboard.countOfTasksWaitingListItemsById(processModelId);
+
+    countOfTasksWaitingListItemsById
+    .then((countOfTasksWaitingListItemsByIds: number) => {
+      expect(countOfTasksWaitingListItemsByIds).toBe(1);
     });
   });
 
   it('should be possbible to click continue in task waiting section.', () => {
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(dashboard.firstTaskWaitingById(processModelId)), taskListTimeoutMS);
-      return dashboard.firstTaskWaitingById(processModelId);
+    const firstTaskWaitingById: ElementFinder = dashboard.firstTaskWaitingById(processModelId);
+
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(firstTaskWaitingById), taskListTimeoutMS);
+      return firstTaskWaitingById;
     });
 
-    dashboard.continueTaskByClickOnButton(processModelId).then(() => {
-      browser.getCurrentUrl().then((currentBrowserUrl: string) => {
-        expect(currentBrowserUrl).toContain(processModel.userTasksInputUrl(processModel.getProcessModelId()));
+    const continueTaskByClickOnButton: any = dashboard.continueTaskByClickOnButton(processModelId);
+    const userTasksInputUrl: string = processModel.userTasksInputUrl(processModelId);
+
+    continueTaskByClickOnButton
+    .then(() => {
+      browser
+      .getCurrentUrl()
+      .then((currentBrowserUrl: string) => {
+        expect(currentBrowserUrl).toContain(userTasksInputUrl);
       });
     });
   });
 
   it('should be possbible to click continue in an opened user task.', () => {
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(dashboard.firstTaskWaitingById(processModelId)), taskListTimeoutMS);
-      return dashboard.firstTaskWaitingById(processModelId);
+    const firstTaskWaitingById: ElementFinder = dashboard.firstTaskWaitingById(processModelId);
+
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(firstTaskWaitingById), taskListTimeoutMS);
+      return firstTaskWaitingById;
     });
 
     dashboard.continueTaskByClickOnButton(processModelId);
 
+    const dynamicUiWrapperContinueButton: ElementFinder = dashboard.dynamicUiWrapperContinueButton;
+
     // Wait until view is loaded and button is visible
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(dashboard.dynamicUiWrapperContinueButton), taskListTimeoutMS);
-      return dashboard.dynamicUiWrapperContinueButton;
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(dynamicUiWrapperContinueButton), taskListTimeoutMS);
+      return dynamicUiWrapperContinueButton;
     });
   });
 
   it('should be in waiting room when click continue in an opened user task.', () => {
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(dashboard.firstTaskWaitingById(processModelId)), taskListTimeoutMS);
-      return dashboard.firstTaskWaitingById(processModelId);
+    const firstTaskWaitingById: ElementFinder = dashboard.firstTaskWaitingById(processModelId);
+
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(firstTaskWaitingById), taskListTimeoutMS);
+      return firstTaskWaitingById;
     });
 
     dashboard.continueTaskByClickOnButton(processModelId);
 
+    const dynamicUiWrapperContinueButton: ElementFinder = dashboard.dynamicUiWrapperContinueButton;
+
     // Wait until view is loaded and button is visible
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.visibilityOf(dashboard.dynamicUiWrapperContinueButton), taskListTimeoutMS);
-      return dashboard.dynamicUiWrapperContinueButton;
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions
+        .visibilityOf(dynamicUiWrapperContinueButton), taskListTimeoutMS);
+      return dynamicUiWrapperContinueButton;
     });
 
+    const correlationId: string = processModel.getCorrelationId();
+    const waitingroomUrl: string = processModel.waitingroomUrl(processModelId, correlationId);
+
     // Should be in waiting room
-    dashboard.continueUserTaskByClickOnDynamicUiWrapperContinuButton().then(() => {
-      browser.getCurrentUrl().then((currentBrowserUrl: string) => {
-        expect(currentBrowserUrl).toContain(processModel.waitingroomUrl(processModelId, processModel.getCorrelationId()));
+    dashboard
+    .continueUserTaskByClickOnDynamicUiWrapperContinuButton()
+    .then(() => {
+      browser
+      .getCurrentUrl()
+      .then((currentBrowserUrl: string) => {
+        expect(currentBrowserUrl).toContain(waitingroomUrl);
       });
     });
 
+    const dashboardLink: string = dashboard.dashboardLink;
+
     // Should be in dashboard view
-    browser.driver.wait(() => {
-      browser.wait(expectedConditions.urlContains(dashboard.dashboardLink), taskListTimeoutMS);
-      return dashboard.dashboardLink;
+    browser.driver
+    .wait(() => {
+      browser
+      .wait(expectedConditions.urlContains(dashboardLink), taskListTimeoutMS);
+      return dashboardLink;
     });
   });
 });
