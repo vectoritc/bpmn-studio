4c94a2240c5a42029b83a1498048fcaf16b6b9c7
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-08-31 16:15:03 +0200

    :fire: Remove Foldable Design of Log Viewer

--- src/modules/inspect-correlation/components/inspect-panel/components/log-viewer/log-viewer.ts
+++ src/modules/inspect-correlation/components/inspect-panel/components/log-viewer/log-viewer.ts
@@ -1,28 +1,5 @@
 import {bindable} from 'aurelia-framework';
 
 export class LogViewer {
-  @bindable({ changeHandler: 'titleChanged' }) public title: string;
-  @bindable({ changeHandler: 'foldableChanged' }) public foldable: string;
   @bindable() public log: string;
-  public showLog: boolean = true;
-  public showHeadline: boolean;
-  public isFoldable: boolean;
-
-  public toggleLogVisibility(): void {
-    if (this.isFoldable) {
-      this.showLog = !this.showLog;
-    }
-  }
-
-  public foldableChanged(): void {
-    this.isFoldable = this.foldable === 'true';
-    this.showHeadline = this.title !== ''
-                     || this.title === undefined
-                     || this.isFoldable;
-  }
-
-  public titleChanged(): void {
-    this.showHeadline = this.title !== ''
-                     || this.isFoldable;
-  }
 }
