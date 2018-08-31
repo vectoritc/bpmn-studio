bb7ea55bef0ce9601f23fbdd9d52dcd0dd03c087
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-08-31 16:31:21 +0200

    :construction: WIP: :sparkles: Add Log Viewer and Process Instance List to Inspect Panel

--- src/modules/inspect-correlation/components/inspect-panel/inspect-panel.ts
+++ src/modules/inspect-correlation/components/inspect-panel/inspect-panel.ts
@@ -1,3 +1,29 @@
-export class InspectPanel {
+import {EventAggregator, Subscription} from 'aurelia-event-aggregator';
+import {inject} from 'aurelia-framework';
+import environment from '../../../../environment';
 
+@inject(EventAggregator)
+export class InspectPanel {
+  public showInspectPanel: boolean;
+
+  private _eventAggregator: EventAggregator;
+  private _subscriptions: Array<Subscription>;
+
+  constructor(eventAggregator: EventAggregator) {
+    this._eventAggregator = eventAggregator;
+  }
+
+  public attached(): void {
+    this._subscriptions = [
+      this._eventAggregator.subscribe(environment.events.inspectView.showInspectPanel, (showInspectPanel: boolean) => {
+        this.showInspectPanel = showInspectPanel;
+      }),
+    ];
+  }
+
+  public detached(): void {
+    for (const subscription of this._subscriptions) {
+      subscription.dispose();
+    }
+  }
 }
