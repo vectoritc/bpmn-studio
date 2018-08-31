2e123014723aa14743ff3de878dd5c6ee5548fd2
dominikkaminski <dominik.kaminski@5minds.de>
2018-08-31 09:31:00 +0200

    Refactor start_docker_bundle.sh

--- test/start_docker_bundle.sh
+++ test/start_docker_bundle.sh
@@ -2,5 +2,6 @@
 
 name='bpmn-studio'
 
-[[ $(docker ps -f "name=$name" --format '{{.Names}}') == $name ]] ||
-docker run --name $name -d -p 8000:8000 -p 9000:9000 5minds/bpmn-studio-bundle:develop 
+if [[ $(docker ps -f "name=$name" --format '{{.Names}}') != "$name"  ]]; then
+    docker run --name "$name" -d -p 8000:8000 -p 9000:9000 5minds/bpmn-studio-bundle:latest 
+fi
