586fa1563ff7be41e9dca809ead3d175cdcce2ea
dominikkaminski <dominik.kaminski@5minds.de>
2018-08-31 10:39:12 +0200

    Add docker container removal when already started

--- test/start_docker_bundle.sh
+++ test/start_docker_bundle.sh
@@ -2,6 +2,9 @@
 
 name='bpmn-studio'
 
-if [[ $(docker ps -f "name=$name" --format '{{.Names}}') != "$name"  ]]; then
+if [[ $(docker ps -f "name=$name" --format '{{.Names}}') != "$name" ]]; then
+    docker run --name "$name" -d -p 8000:8000 -p 9000:9000 5minds/bpmn-studio-bundle:latest 
+else
+    docker rm -v -f "$name"
     docker run --name "$name" -d -p 8000:8000 -p 9000:9000 5minds/bpmn-studio-bundle:latest 
 fi
