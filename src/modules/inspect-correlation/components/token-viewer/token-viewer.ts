8300d28e0b00574adbe1c16b1f08d68a64c3c471
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-09-05 16:30:20 +0200

    :lipstick: Improve Style of Token Viewer

--- src/modules/inspect-correlation/components/token-viewer/token-viewer.ts
+++ src/modules/inspect-correlation/components/token-viewer/token-viewer.ts
@@ -1,5 +1,13 @@
 import {bindable} from 'aurelia-framework';
 
 export class TokenViewer {
-  @bindable() public token: string;
+  @bindable({ changeHandler: 'formatToken' }) public token: string;
+  public formattedToken: string;
+
+  public formatToken(): void {
+    const unformattedToken: JSON = JSON.parse(this.token);
+
+    // tslint:disable-next-line:no-magic-numbers
+    this.formattedToken = JSON.stringify(unformattedToken, null, 2);
+  }
 }
