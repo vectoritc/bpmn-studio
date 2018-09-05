233a9e4613d4f7b11e0eeab8637f1801fbd72807
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-09-05 15:35:36 +0200

    :fire: Remove Foldable Design of Token Viewer

--- src/modules/inspect-correlation/components/token-viewer/token-viewer.ts
+++ src/modules/inspect-correlation/components/token-viewer/token-viewer.ts
@@ -1,28 +1,5 @@
 import {bindable} from 'aurelia-framework';
 
 export class TokenViewer {
-  @bindable({ changeHandler: 'titleChanged' }) public title: string;
-  @bindable({ changeHandler: 'foldableChanged' }) public foldable: string;
   @bindable() public token: string;
-  public showToken: boolean = true;
-  public showHeadline: boolean;
-  public isFoldable: boolean;
-
-  public toggleTokenVisibility(): void {
-    if (this.isFoldable) {
-      this.showToken = !this.showToken;
-    }
-  }
-
-  public foldableChanged(): void {
-    this.isFoldable = this.foldable === 'true';
-    this.showHeadline = this.title !== ''
-                     || this.title === undefined
-                     || this.isFoldable;
-  }
-
-  public titleChanged(): void {
-    this.showHeadline = this.title !== ''
-                     || this.isFoldable;
-  }
 }
