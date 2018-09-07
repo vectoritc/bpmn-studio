250edb9a875b709ef2dc6bef885e27735e5d645c
bastian schnittger <bastian.schnittger@5minds.de>
2018-09-07 09:48:46 +0200

    Add update check to dashboard

--- src/modules/dashboard/dashboard.ts
+++ src/modules/dashboard/dashboard.ts
@@ -213,7 +213,13 @@ export class Dashboard {
 
   private async _updateUserTasks(): Promise<void> {
     try {
-      this._userTasks = await this._getUserTasks();
+      const userTasks: Array<IUserTaskWithProcessModel> = await this._getUserTasks();
+      const userTaskListWasUpdatet: boolean = JSON.stringify(userTasks) !== JSON.stringify(this._userTasks);
+
+      if (userTaskListWasUpdatet) {
+        this._userTasks = userTasks;
+      }
+
       this.successfullyRequested = true;
     } catch (error) {
       if (isError(error, UnauthorizedError)) {
