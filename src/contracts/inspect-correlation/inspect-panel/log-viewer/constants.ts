9148e81194988c399b71bb3e92f3b0c162e55ce3
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-09-10 12:56:47 +0200

    :sparkles: Make Log Table Sortable

---
+++ src/contracts/inspect-correlation/inspect-panel/log-viewer/constants.ts
@@ -0,0 +1,5 @@
+export enum LogSortProperty {
+  Time = 'timestamp',
+  Message = 'message',
+  LogLevel = 'logLevel',
+}
--- src/modules/inspect-correlation/components/inspect-panel/components/log-viewer/log-viewer.ts
+++ src/modules/inspect-correlation/components/inspect-panel/components/log-viewer/log-viewer.ts
@@ -2,13 +2,19 @@ import {bindable, inject} from 'aurelia-framework';
 
 import * as clipboard from 'clipboard-polyfill';
 
-import {ILogEntry, NotificationType} from '../../../../../../contracts/index';
+import {ILogEntry, ILogSortSettings, LogSortProperty, NotificationType} from '../../../../../../contracts/index';
 import {DateService} from '../../../../../date-service/date.service';
 import {NotificationService} from '../../../../../notification/notification.service';
 
 @inject('NotificationService')
 export class LogViewer {
-  @bindable() public log: Array<ILogEntry>;
+  @bindable({ changeHandler: 'logChanged' }) public log: Array<ILogEntry>;
+  public LogSortProperty: typeof LogSortProperty = LogSortProperty;
+  public sortedLog: Array<ILogEntry>;
+  public sortSettings: ILogSortSettings = {
+    ascending: false,
+    sortProperty: undefined,
+  };
 
   private _dateService: DateService;
   private _notificationService: NotificationService;
@@ -16,6 +22,10 @@ export class LogViewer {
   constructor(notificationService: NotificationService) {
     this._notificationService = notificationService;
     this._dateService = new DateService();
+  }
+
+  public logChanged(): void {
+    this.sortList(LogSortProperty.Time);
   }
 
   public copyToClipboard(textToCopy: string): void {
@@ -28,5 +38,39 @@ export class LogViewer {
     const dateString: string = this._dateService.getDateStringFromTimestamp(timestamp);
 
     return dateString;
+  }
+
+  public sortList(property: LogSortProperty): void {
+    this.sortedLog = [];
+    const isSamePropertyAsPrevious: boolean = this.sortSettings.sortProperty === property;
+    const ascending: boolean = isSamePropertyAsPrevious ? !this.sortSettings.ascending
+                                                        : true;
+
+    this.sortSettings.ascending = ascending;
+    this.sortSettings.sortProperty = property;
+
+    const sortedLog: Array<ILogEntry> = this._getSortedLogByProperty(property);
+
+    this.sortedLog = ascending ? sortedLog
+                                     : sortedLog.reverse();
+  }
+
+  private _getSortedLogByProperty(property: LogSortProperty): Array<ILogEntry> {
+    const sortedLog: Array<ILogEntry> =
+      this.log.sort((firstEntry: ILogEntry, secondEntry: ILogEntry) => {
+        const firstEntryIsBigger: boolean = firstEntry[property] > secondEntry[property];
+        if (firstEntryIsBigger) {
+          return 1;
+        }
+
+        const secondEntryIsBigger: boolean = firstEntry[property] < secondEntry[property];
+        if (secondEntryIsBigger) {
+          return -1;
+        }
+
+        return 0;
+      });
+
+    return sortedLog;
   }
 }
