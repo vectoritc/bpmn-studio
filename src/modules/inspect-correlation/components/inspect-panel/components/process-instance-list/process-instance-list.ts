ebb06a4d7a179ed5a8b8cad2d20e56ced6479714
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-09-12 12:25:07 +0200

    :lipstick: Make State Start With Uppercase

--- src/modules/inspect-correlation/components/inspect-panel/components/process-instance-list/process-instance-list.ts
+++ src/modules/inspect-correlation/components/inspect-panel/components/process-instance-list/process-instance-list.ts
@@ -41,10 +41,12 @@ export class ProcessInstanceList {
     for (const correlation of correlations) {
       const formattedStartedDate: string = this._dateService.getDateStringFromTimestamp(correlation.startedAt);
 
+      const state: string = correlation.state.charAt(0).toUpperCase() + correlation.state.slice(1);
+
       const tableEntry: IProcessInstanceTableEntry = {
         index: this._getIndexForCorrelation(correlation, correlations),
         startedAt: formattedStartedDate,
-        state: correlation.state,
+        state: state,
         user: correlation.user,
         correlationId: correlation.id,
       };
