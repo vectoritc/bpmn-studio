fa3620dcd996ad187364e12e3c87988068e4285e
Paul <paul.heidenreich@5minds.de>
2018-09-13 08:49:05 +0200

    :construction_worker: Wait on Required Resources Before Running Tests

--- test/jenkins_e2e.sh
+++ test/jenkins_e2e.sh
@@ -7,4 +7,9 @@ node node_modules/webdriver-manager start &
 
 npm start -- --port=9000 &
 
+# Wait for required resources to be up and running.
+while ! curl --silent localhost:8000 > /dev/null; do sleep 1; done
+while ! curl --silent localhost:4444 > /dev/null; do sleep 1; done
+while ! curl --silent localhost:9000 > /dev/null; do sleep 1; done
+
 npm run jenkins-run-end-to-end-tests
