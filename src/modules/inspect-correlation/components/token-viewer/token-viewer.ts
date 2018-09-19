42375c675c2882de1a5c48b13e858967bf368d78
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-09-19 16:53:55 +0200

    :construction: WIP: :sparkles: Adjust Token Viewer to Backend

--- src/modules/inspect-correlation/components/token-viewer/token-viewer.ts
+++ src/modules/inspect-correlation/components/token-viewer/token-viewer.ts
@@ -1,8 +1,25 @@
-import {bindable} from 'aurelia-framework';
+import {bindable, inject} from 'aurelia-framework';
 
+import {Correlation} from '@process-engine/management_api_contracts';
+
+import {IInspectCorrelationService} from '../../contracts';
+
+@inject('InspectCorrelationService')
 export class TokenViewer {
-  @bindable({ changeHandler: 'formatToken' }) public token: string;
+  @bindable({ changeHandler: 'correlationChanged' }) public correlation: Correlation;
+  @bindable() public token: string;
   public formattedToken: string;
+
+  private _inspectCorrelationService: IInspectCorrelationService;
+
+  constructor(inspectCorrelationService: IInspectCorrelationService) {
+    this._inspectCorrelationService = inspectCorrelationService;
+  }
+
+  public async correlationChanged(): Promise<void> {
+    this.token = await this._inspectCorrelationService.getTokenForCorrelation(this.correlation);
+    this.formatToken();
+  }
 
   public formatToken(): void {
     const unformattedToken: JSON = JSON.parse(this.token);
