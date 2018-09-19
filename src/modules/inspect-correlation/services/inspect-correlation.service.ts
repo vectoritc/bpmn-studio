b81e2aeb1401835160b62ff6205c79182a8a684f
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-09-19 16:40:17 +0200

    :construction: WIP: :sparkles: Adjust Inspect Correlation Service to Backend

--- src/modules/inspect-correlation/services/inspect-correlation.service.ts
+++ src/modules/inspect-correlation/services/inspect-correlation.service.ts
@@ -2,6 +2,7 @@ import {inject} from 'aurelia-framework';
 
 import {Correlation} from '@process-engine/management_api_contracts';
 
+import {LogEntry} from '@process-engine/logging_api_contracts';
 import {IInspectCorrelationRepository, IInspectCorrelationService} from '../contracts';
 
 @inject('InspectCorrelationRepository')
@@ -14,5 +15,13 @@ export class InspectCorrelationService implements IInspectCorrelationService {
 
   public getAllCorrelationsForProcessModelId(processModelId: string): Promise<Array<Correlation>> {
     return this._inspectCorrelationRepository.getAllCorrelationsForProcessModelId(processModelId);
+  }
+
+  public getLogsForCorrelation(correlation: Correlation): Promise<Array<LogEntry>> {
+    return this._inspectCorrelationRepository.getLogsForCorrelation(correlation);
+  }
+
+  public getTokenForCorrelation(correlation: Correlation): Promise<string> {
+    return this._inspectCorrelationRepository.getTokenForCorrelation(correlation);
   }
 }
