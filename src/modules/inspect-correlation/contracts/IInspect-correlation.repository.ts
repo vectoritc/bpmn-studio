702c384d051f4f5e46d306db543dbca91c785d40
Steffen Knaup <Steffen.Knaup@5minds.de>
2018-09-19 16:40:50 +0200

    :construction: WIP: :sparkles: Adjust Interfaces to Backend

--- src/modules/inspect-correlation/contracts/IInspect-correlation.repository.ts
+++ src/modules/inspect-correlation/contracts/IInspect-correlation.repository.ts
@@ -1,5 +1,8 @@
+import {LogEntry} from '@process-engine/logging_api_contracts';
 import {Correlation} from '@process-engine/management_api_contracts';
 
 export interface IInspectCorrelationRepository {
   getAllCorrelationsForProcessModelId(processModelId: string): Promise<Array<Correlation>>;
+  getLogsForCorrelation(correlation: Correlation): Promise<Array<LogEntry>>;
+  getTokenForCorrelation(correlation: Correlation): Promise<string>;
 }
