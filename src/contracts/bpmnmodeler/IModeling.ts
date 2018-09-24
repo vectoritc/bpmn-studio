e733ffcaa109387f727353cb084b9aac7b3276f1
Steffen <Steffen.knaup@5minds.de>
2018-09-24 14:07:52 +0200

    :lipstick: Extend IModeling

--- src/contracts/bpmnmodeler/IModeling.ts
+++ src/contracts/bpmnmodeler/IModeling.ts
@@ -2,6 +2,7 @@ import {IShape} from './index';
 
 export interface IModeling {
   updateProperties(element: IShape, properties: object): void;
+  updateLabel(element: IShape, name: string): void;
   setColor(elements: Array<IShape> | IShape, options: {
     fill: string,
     stroke: string,
