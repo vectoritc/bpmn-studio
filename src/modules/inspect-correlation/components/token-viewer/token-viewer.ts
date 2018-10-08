70bfd127ccb9852a14e51750324eee5b6eaacb27
Steffen <Steffen.knaup@5minds.de>
2018-10-08 13:49:40 +0200

    :lipstick: Improve Code Quality

--- src/modules/inspect-correlation/components/token-viewer/token-viewer.ts
+++ src/modules/inspect-correlation/components/token-viewer/token-viewer.ts
@@ -27,12 +27,13 @@ export class TokenViewer {
     this.tokenEntries = [];
 
     // Check if the selected Element can have a token.
-    const elementHasNoToken: boolean = this.flowNode.type.includes('Lane') ||
-                                      this.flowNode.type.includes('Collaboration') ||
-                                      this.flowNode.type.includes('Participant');
+    const elementHasNoToken: boolean = this.flowNode.type.includes('Lane')
+                                    || this.flowNode.type.includes('Collaboration')
+                                    || this.flowNode.type.includes('Participant');
 
     if (elementHasNoToken) {
       this.showTokenEntries = false;
+
       return;
     }
 
