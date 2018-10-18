7ff6a9e1b1ea44cd0fb5954c7f781eb30fcc781f
robpal94 <robin.palkovits@5minds.de>
2018-10-18 15:44:37 +0200

    :sparkles: add a link to the release notes

--- electron_app/electron.js
+++ electron_app/electron.js
@@ -496,6 +496,12 @@ Main._createMainWindow = function () {
           const documentation_url = 'https://www.process-engine.io/documentation/';
           electron.shell.openExternal(documentation_url);
         }
+      }, {
+        label: "Release Notes",
+          click: () => {
+            const currentReleaseNotesUrl = 'https://github.com/process-engine/bpmn-studio/releases/latest'
+            electron.shell.openExternal(currentReleaseNotesUrl);
+          }
       }]
     }];
 
