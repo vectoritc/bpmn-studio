2f96f47093bdcbe4b49365c985ef6f4547e4cb8f
bastian schnittger <bastian.schnittger@5minds.de>
2018-11-27 16:12:17 +0100

    add some mire functionallity to new service

--- src/contracts/solution-explorer/IActiveSolutionAndDiagramService.ts
+++ src/contracts/solution-explorer/IActiveSolutionAndDiagramService.ts
@@ -3,7 +3,38 @@ import {IDiagram} from '@process-engine/solutionexplorer.contracts';
 import {ISolutionEntry} from './ISolutionEntry';
 
 export interface IActiveSolutionAndDiagramService {
-  getActiveSolution(): ISolutionEntry;
+  /**
+   * Adds a SolutionEntry to the service.
+   * @param solutionEntry The SolutionEntry to add.
+   */
+  addSolutionEntry(solutionEntry: ISolutionEntry): void;
+
+  /**
+   * Closes the given SolutionEntry and deletes it from the service.
+   * @param solutionEntry The SolutionEntry to close.
+   */
+  closeSolutionEntry(solutionEntry: ISolutionEntry): void;
+
+  /**
+   * Gets the currently active SolutionEntry.
+   */
+  getActiveSolutionEntry(): ISolutionEntry;
+
+  /**
+   * Gets the currently active diagram.
+   */
   getActiveDiagram(): IDiagram;
+
+  /**
+   * Gets a specific SolutionEntry identified by its uri.
+   * @param uri The uri of the searched SolutionEntry.
+   */
+  getSolutionEntryForUri(uri: string): ISolutionEntry;
+
+  /**
+   * Sets the combination of the new SolutionEntry and diagram.
+   * @param solution The new active SolutionEntry.
+   * @param diagram The new active diagram.
+   */
   setActiveSolutionAndDiagram(solution: ISolutionEntry, diagram: IDiagram): void;
 }
