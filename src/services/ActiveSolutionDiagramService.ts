4687d86d2292dae11a3b07da1dcfe27325b589e8
bastian schnittger <bastian.schnittger@5minds.de>
2018-11-27 16:12:32 +0100

    implement those functions

--- src/services/ActiveSolutionDiagramService.ts
+++ src/services/ActiveSolutionDiagramService.ts
@@ -2,15 +2,34 @@ import {IDiagram} from '@process-engine/solutionexplorer.contracts';
 import {IActiveSolutionAndDiagramService, ISolutionEntry} from '../contracts';
 
 export class ActiveSolutionAndDiagramService implements IActiveSolutionAndDiagramService {
+  private _allSolutionEntries: Array<ISolutionEntry> = [];
   private _activeSolution: ISolutionEntry;
   private _activeDiagram: IDiagram;
 
-  public getActiveSolution(): ISolutionEntry {
+  public addSolutionEntry(solutionEntry: ISolutionEntry): void {
+    this._allSolutionEntries.push(solutionEntry);
+  }
+
+  public closeSolutionEntry(solutionEntry: ISolutionEntry): void {
+    this._allSolutionEntries.splice(this._allSolutionEntries.indexOf(solutionEntry));
+  }
+
+  public getActiveSolutionEntry(): ISolutionEntry {
+
     return this._activeSolution;
   }
 
   public getActiveDiagram(): IDiagram {
+
     return this._activeDiagram;
+  }
+
+  public getSolutionEntryForUri(uri: string): ISolutionEntry {
+    const solutionEntry: ISolutionEntry = this._allSolutionEntries.find((entry: ISolutionEntry) => {
+      return entry.uri === uri;
+    });
+
+    return solutionEntry;
   }
 
   public setActiveSolutionAndDiagram(solution: ISolutionEntry, diagram: IDiagram): void {
