90bfac0d2bd3193f2bcbb856c3d07971ef49e236
Steffen <Steffen.knaup@5minds.de>
2018-12-13 16:04:40 +0100

    :sparkles: Add a Way to Enter The Authtoken

--- dev_tooling/scripts/generate_changelog/create-changelog.sh
+++ dev_tooling/scripts/generate_changelog/create-changelog.sh
@@ -5,16 +5,16 @@
 #####
 
 if [ $# -ne 2 ]; then
-  echo "Usage: $0 <Previous_Version> <Current_Version>"
+  echo "Usage: $0 <Github Username>:<Github Authtoken> <Previous_Version> <Current_Version>"
 
   exit 1
 fi
 
 echo " ️  Generating Changelog File..."
-bash generate_release_markdown_file.sh $1 $2
+bash generate_release_markdown_file.sh $2 $3
 
 echo " ️  Getting Merge Commits.."
-bash get_merge_commits.sh $1 $2
+bash get_merge_commits.sh $2 $3
 bash format_messages.bash merge_commits_of_release.txt formatted_messages.txt
 
 echo "🛀 Please clean up the Merge Commits!"
@@ -23,17 +23,17 @@ bash sort_and_format_merge_commits.sh
 
 echo "📋 Merge Commits have been copied to clipboard."
 echo "✏️  Please paste the Merge Commits into the 'Full Changelog' section!"
-code releasenotes_$2.md --wait
+code releasenotes_$3.md --wait
 
 echo " ️ Getting Closed Issues.."
-GITHUB_AUTH="" bash get_fixed_issues.sh
+GITHUB_AUTH="$1" bash get_fixed_issues.sh
 echo "🛀 Please clean up the Closed Issues!"
 code closed_issues --wait
 bash format_messages.bash closed_issues formatted_closed_issues.txt
 echo "📋 Closed Issues have been copied to Clipboard."
 cat formatted_closed_issues.txt | pbcopy
 echo "✏️  Please paste the Closed Issues in the 'Fixed Issues' section!"
-code releasenotes_$2.md --wait
+code releasenotes_$3.md --wait
 echo "  Release Notes have been created!!!"
-code releasenotes_$2.md --wait
+code releasenotes_$3.md --wait
 echo "👋 Bye Bye!"
