4447608520f266ae5a3b8070f22716d754fc4301
Vyperus <bastian.schnittger@5minds.de>
2018-12-27 15:29:30 +0100

    :recycle: Remove Solutions Identified by its uri

--- src/contracts/solution-explorer/ISolutionService.ts
+++ src/contracts/solution-explorer/ISolutionService.ts
@@ -10,12 +10,6 @@ export interface ISolutionService {
   addSolutionEntry(solutionEntry: ISolutionEntry): void;
 
   /**
-   * Closes the given SolutionEntry and deletes it from the service.
-   * @param solutionEntry The SolutionEntry to close.
-   */
-  removeSolutionEntry(solutionEntry: ISolutionEntry): void;
-
-  /**
    * Gets a specific SolutionEntry identified by its URI.
    * @param uri The uri of the searched SolutionEntry.
    */
@@ -25,4 +19,10 @@ export interface ISolutionService {
    * Gets a list of all persisted solutions from the last session.
    */
   getPersistedEntries(): Array<ISolutionEntry>;
+
+  /**
+   * Deletes a specific solution indetified by its id.
+   * @param uri The uri of the solution entry to delete.
+   */
+  removeSolutionEntryByUri(uri: string): void;
 }
