d904ea27856056e34b6fed6327b64b3fc9d409d5
Vyperus <bastian.schnittger@5minds.de>
2018-12-28 08:51:48 +0100

    :sparkles: Add Solution Service to Think View

--- src/modules/think/processdef-list/processdef-list.ts
+++ src/modules/think/processdef-list/processdef-list.ts
@@ -8,17 +8,18 @@ import {Router} from 'aurelia-router';
 import {ForbiddenError, isError, UnauthorizedError} from '@essential-projects/errors_ts';
 import {IIdentity} from '@essential-projects/iam_contracts';
 import {IManagementApi, ProcessModelExecution} from '@process-engine/management_api_contracts';
+import {IDiagram, ISolution} from '@process-engine/solutionexplorer.contracts';
 
-import {AuthenticationStateEvent, IAuthenticationService, ISolutionService, NotificationType} from '../../../contracts/index';
+import {AuthenticationStateEvent, IAuthenticationService, ISolutionEntry, ISolutionService, NotificationType} from '../../../contracts/index';
 import environment from '../../../environment';
 import {NotificationService} from '../../notification/notification.service';
 
-@inject(EventAggregator, Router, 'AuthenticationService', 'ManagementApiClientService', 'NotificationService')
 interface RouteParameters {
   diagramName?: string;
   solutionUri?: string;
 }
 
+@inject(EventAggregator, Router, 'AuthenticationService', 'ManagementApiClientService', 'NotificationService', 'SolutionService')
 export class ProcessDefList {
 
   public allProcessModels: Array<ProcessModelExecution.ProcessModel>;
@@ -30,17 +31,21 @@ export class ProcessDefList {
   private _router: Router;
   private _subscriptions: Array<Subscription>;
   private _getProcessesIntervalId: number;
+  private _activeSolutionEntry: ISolutionEntry;
+  private _solutionService: ISolutionService;
 
   constructor(eventAggregator: EventAggregator,
               router: Router,
               authenticationService: IAuthenticationService,
               managementApiClient: IManagementApi,
-              notificationService: NotificationService) {
+              notificationService: NotificationService,
+              solutionService: ISolutionService) {
     this._eventAggregator = eventAggregator;
     this._router = router;
     this._authenticationService = authenticationService;
     this._managementApiClient = managementApiClient;
     this._notificationService = notificationService;
+    this._solutionService = solutionService;
 
     this._eventAggregator.publish(environment.events.refreshProcessDefs);
   }
