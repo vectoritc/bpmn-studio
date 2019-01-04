4dd6ee212fb605d4c4b0656f08ca97ee74d524f7
robpal94 <robin.palkovits@5minds.de>
2019-01-04 09:57:44 +0100

    :art: remove console log

--- src/modules/design/diagram-detail/diagram-detail.ts
+++ src/modules/design/diagram-detail/diagram-detail.ts
@@ -131,7 +131,6 @@ export class DiagramDetail {
         this.showStartWithOptionsModal = true;
       }),
       this._eventAggregator.subscribe(environment.events.diagramDetail.suppressUnsavedChangesModal, () => {
-        console.log('suppress');
         this._suppressSaveChangesModal = true;
       }),
     ];
