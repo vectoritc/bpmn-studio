e42b584eb7766c0428759ab8412e1c6f3b101d51
Vyperus <bastian.schnittger@5minds.de>
2019-01-08 16:16:20 +0100

    :bug: Correct type of all Fields

--- src/modules/design/bpmn-io/bpmn-io.ts
+++ src/modules/design/bpmn-io/bpmn-io.ts
@@ -368,7 +368,7 @@ export class BpmnIo {
   }
 
   private _renameFormFields(event: IInternalEvent): IInternalEvent {
-    const allFields: Array<IModdleElement> = event.descriptor.businessObject.extensionElements.values[0].fields;
+    const allFields: Array<IPropertiesElement> = event.descriptor.businessObject.extensionElements.values;
 
     const formFields: Array<IModdleElement> = allFields.filter((field: IModdleElement) => {
       return field.$type === 'camunda:FormField';
