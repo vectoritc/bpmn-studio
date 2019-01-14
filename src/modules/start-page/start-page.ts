13ab4900b27698376675e8898bc036a4d26333b5
Vyperus <bastian.schnittger@5minds.de>
2019-01-14 14:59:20 +0100

    :sparkles: Add Function to fire Event

--- src/modules/start-page/start-page.ts
+++ src/modules/start-page/start-page.ts
@@ -1,6 +1,8 @@
 import {EventAggregator} from 'aurelia-event-aggregator';
 import {inject} from 'aurelia-framework';
 
+import environment from '../../environment';
+
 @inject(EventAggregator)
 export class StartPage {
   private _eventAggregator: EventAggregator;
@@ -13,4 +15,7 @@ export class StartPage {
     this._eventAggregator = eventAggregator;
   }
 
+  public openLocalSolution(): void {
+    this._eventAggregator.publish(environment.events.startPage.openLocalSolution);
+  }
 }
