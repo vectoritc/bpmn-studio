a99d460534061aee5f8282dec7c44e201343c806
Vyperus <bastian.schnittger@5minds.de>
2019-01-14 14:59:04 +0100

    :sparkles: Add Checks for Operating Systems

--- src/modules/start-page/start-page.ts
+++ src/modules/start-page/start-page.ts
@@ -4,6 +4,11 @@ import {inject} from 'aurelia-framework';
 @inject(EventAggregator)
 export class StartPage {
   private _eventAggregator: EventAggregator;
+
+  public isRunningInElectron: boolean = (window as any).nodeRequire;
+  public isRunningOnWindows: boolean = process.platform === 'win32';
+  public isRunningOnMacOS: boolean = process.platform === 'darwin';
+
   constructor(eventAggregator: EventAggregator) {
     this._eventAggregator = eventAggregator;
   }
