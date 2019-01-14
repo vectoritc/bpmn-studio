2ceb0149f8acdcfa2f5c88808146095a71be8d4e
Alex😎 <alexander.kasten@5minds.de>
2019-01-14 15:54:14 +0100

    :sparkles: Add setIdentity Method to Heatmap Repository Contracts

--- src/modules/inspect/heatmap/contracts/IHeatmap.Repository.ts
+++ src/modules/inspect/heatmap/contracts/IHeatmap.Repository.ts
@@ -1,3 +1,4 @@
+import {IIdentity} from '@essential-projects/iam_contracts';
 import {ActiveToken, FlowNodeRuntimeInformation} from '@process-engine/kpi_api_contracts';
 import {ProcessModelExecution} from '@process-engine/management_api_contracts';
 
@@ -5,4 +6,5 @@ export interface IHeatmapRepository {
   getRuntimeInformationForProcessModel(processModelId: string): Promise<Array<FlowNodeRuntimeInformation>>;
   getProcess(processModelId: string): Promise<ProcessModelExecution.ProcessModel>;
   getActiveTokensForFlowNode(flowNodeId: string): Promise<Array<ActiveToken>>;
+  setIdentity(identity: IIdentity): void;
 }
