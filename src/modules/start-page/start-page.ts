1c215b851bdc750dd8367fb1ef62ae9ce5f14840
Vyperus <bastian.schnittger@5minds.de>
2019-01-14 14:58:50 +0100

    :sparkles: Inject EventAggregator

--- src/modules/start-page/start-page.ts
+++ src/modules/start-page/start-page.ts
@@ -1,3 +1,11 @@
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {inject} from 'aurelia-framework';
+
+@inject(EventAggregator)
 export class StartPage {
+  private _eventAggregator: EventAggregator;
+  constructor(eventAggregator: EventAggregator) {
+    this._eventAggregator = eventAggregator;
+  }
 
 }
