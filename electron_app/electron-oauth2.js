6bdcf024dd83ed7af8ae43952aff802eaf63acf7
Vyperus <bastian.schnittger@5minds.de>
2019-01-16 16:12:20 +0100

    :sparkles: Prepare Auth Url

--- electron_app/electron-oauth2.js
+++ electron_app/electron-oauth2.js
@@ -1,3 +1,24 @@
 'use strict';
 module.exports = function (config, windowParams) {
+  function getAuthorizationCode(opts) {
+    opts = opts || {};
+
+    var urlParams = {
+      client_id: config.clientId,
+      redirect_uri: config.redirectUri,
+      response_type: config.responseType,
+      scope: config.scope,
+      state: config.state,
+      nonce: config.nonce,
+    };
+
+    if (opts.scope) {
+      urlParams.scope = opts.scope;
+    }
+
+    if (opts.accessType) {
+      urlParams.access_type = opts.accessType;
+    }
+
+    var url = config.authorizationUrl + '?' + queryString.stringify(urlParams);
 };
