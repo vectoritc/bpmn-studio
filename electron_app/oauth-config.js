bacfee9c23b8a9fb05bfe540a0ce9d8ea9af4d5d
Vyperus <bastian.schnittger@5minds.de>
2019-01-16 16:10:14 +0100

    :sparkles: Introdcue Simple Config

---
+++ electron_app/oauth-config.js
@@ -0,0 +1,9 @@
+module.exports =  {
+  authorizationUrl: 'http://localhost:5000/connect/authorize',
+  clientId: 'bpmn_studio',
+  redirectUri: 'http://localhost:9000/signin-oidc',
+  responseType: 'id_token token',
+  scope: 'openid profile test_resource',
+  state: '123e92f955134138858c4e5d290fb0c5&',
+  nonce: 'e56f9c3f2a13484a89b75bce2866d780'
+}
