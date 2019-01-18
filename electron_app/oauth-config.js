c992c9dfc1b01a2d10aa27884d7caa7542d608b0
Vyperus <bastian.schnittger@5minds.de>
2019-01-18 10:54:28 +0100

    :sparkles: Add Needed Configs

--- electron_app/oauth-config.js
+++ electron_app/oauth-config.js
@@ -1,7 +1,9 @@
 module.exports =  {
+  logoutUrl: 'http://localhost:5000/connect/endsession',
   authorizationUrl: 'http://localhost:5000/connect/authorize',
   clientId: 'bpmn_studio',
   redirectUri: 'http://localhost:9000/signin-oidc',
+  logoutRedirectUri: 'http://localhost:9000/signout-oidc',
   responseType: 'id_token token',
   scope: 'openid profile test_resource',
   state: '123e92f955134138858c4e5d290fb0c5&',
