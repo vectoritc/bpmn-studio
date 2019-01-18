6b0bce1ea43cf3c0035c66b8d8dc4f5f6352761f
Vyperus <bastian.schnittger@5minds.de>
2019-01-18 10:54:36 +0100

    :memo: Add Helpful Comment

--- electron_app/oauth-config.js
+++ electron_app/oauth-config.js
@@ -1,3 +1,12 @@
+/**
+ * We are redirecting to localhost:9000 even on electron here.
+ * The reason behind this is the security of chrome that marks every redirect
+ * to a custom protocol like "bpmn-studio:/" as "unsafe redirect".
+ *
+ * Even though this is the redirect route we never truly redirect there since
+ * we intercept the redirect request in the implementation.
+ */
+
 module.exports =  {
   logoutUrl: 'http://localhost:5000/connect/endsession',
   authorizationUrl: 'http://localhost:5000/connect/authorize',
