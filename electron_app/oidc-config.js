ef3c4a3005225452f7bf2cef2d6d2f1214a27bc8
Vyperus <bastian.schnittger@5minds.de>
2019-01-21 10:23:03 +0100

    :recycle: Name File Correctly

--- electron_app/oidc-config.js
+++ electron_app/oidc-config.js
@@ -15,6 +15,4 @@ module.exports =  {
   logoutRedirectUri: 'http://localhost:9000/signout-oidc',
   responseType: 'id_token token',
   scope: 'openid profile test_resource',
-  state: '123e92f955134138858c4e5d290fb0c5&',
-  nonce: 'e56f9c3f2a13484a89b75bce2866d780'
 }
