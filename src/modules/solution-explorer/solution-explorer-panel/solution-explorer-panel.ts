347eaf2bf81483037a57920ea05564344adaa005
Vyperus <bastian.schnittger@5minds.de>
2019-01-22 11:31:26 +0100

    :memo: Add a Comment

--- src/modules/solution-explorer/solution-explorer-panel/solution-explorer-panel.ts
+++ src/modules/solution-explorer/solution-explorer-panel/solution-explorer-panel.ts
@@ -208,13 +208,16 @@ export class SolutionExplorerPanel {
       return true;
     }
 
-    const pattern: RegExp = new RegExp('^(https?:\\/\\/)?' +
+    /**
+     * This RegEx checks if the entered URI is valid or not.
+     */
+    const urlRegEx: RegExp = new RegExp('^(https?:\\/\\/)?' +
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' +
     '((\\d{1,3}\\.){3}\\d{1,3}))' +
     '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
     '(\\?[;&amp;a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$', 'i');
 
-    const uriIsValid: boolean = pattern.test(this.uriOfRemoteSolution);
+    const uriIsValid: boolean = urlRegEx.test(this.uriOfRemoteSolution);
 
     return uriIsValid;
   }
