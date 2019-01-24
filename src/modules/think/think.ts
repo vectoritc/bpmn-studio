02b02e97db81c86c0d056cc0b8b584410ecd43f9
Alex😎 <alexander.kasten@5minds.de>
2019-01-24 12:17:30 +0100

    :bug: Fix Service Of Undefined Bug On Think

--- src/modules/think/think.ts
+++ src/modules/think/think.ts
@@ -1,6 +1,7 @@
 import {inject} from 'aurelia-framework';
 
-import {ISolutionEntry, ISolutionService} from '../../contracts';
+import {ISolutionEntry, ISolutionService, NotificationType} from '../../contracts/index';
+import {NotificationService} from '../notification/notification.service';
 
 export interface IThinkRouteParameters {
   view?: string;
@@ -8,19 +9,21 @@ export interface IThinkRouteParameters {
   solutionUri?: string;
 }
 
-@inject('SolutionService')
+@inject('SolutionService', 'NotificationService')
 export class Think {
   public showDiagramList: boolean = false;
 
   public activeSolutionEntry: ISolutionEntry;
 
   private _solutionService: ISolutionService;
+  private _notificationService: NotificationService;
 
-  constructor(solutionService: ISolutionService) {
+  constructor(solutionService: ISolutionService, notificationService: NotificationService) {
     this._solutionService = solutionService;
+    this._notificationService = notificationService;
   }
 
-  public async activate(routeParameters: IThinkRouteParameters): Promise<void> {
+  public async canActivate(routeParameters: IThinkRouteParameters): Promise<boolean> {
     const solutionUriIsSet: boolean = routeParameters.solutionUri !== undefined;
 
     const solutionUri: string = solutionUriIsSet
@@ -28,8 +31,18 @@ export class Think {
                               : window.localStorage.getItem('InternalProcessEngineRoute');
 
     this.activeSolutionEntry = this._solutionService.getSolutionEntryForUri(solutionUri);
+    const noActiveSolution: boolean = this.activeSolutionEntry === undefined;
+    if (noActiveSolution) {
+      this._notificationService.showNotification(NotificationType.INFO, 'Please open a solution first.');
+      return false;
+    }
+
     await this.activeSolutionEntry.service.openSolution(this.activeSolutionEntry.uri, this.activeSolutionEntry.identity);
 
+    return true;
+  }
+
+  public activate(): void {
     this.showDiagramList = true;
   }
 }
