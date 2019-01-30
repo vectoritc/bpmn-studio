842a09c7d6ee61d7dac4e35b3abfce854b934cdd
Steffen <Steffen.knaup@5minds.de>
2019-01-30 10:48:33 +0100

    :lipstick: Update Imports in Token-Viewer

--- src/modules/inspect/token-viewer/token-viewer.ts
+++ src/modules/inspect/token-viewer/token-viewer.ts
@@ -3,8 +3,8 @@ import {bindable, inject} from 'aurelia-framework';
 import {DataModels} from '@process-engine/management_api_contracts';
 import {IDiagram} from '@process-engine/solutionexplorer.contracts';
 
-import {IShape} from '../../../../../contracts';
-import {IInspectCorrelationService, IPayloadEntry, IPayloadEntryValue, IRawTokenEntry, ITokenEntry} from '../../contracts';
+import {IShape} from '../../../contracts';
+import {IInspectCorrelationService, IPayloadEntry, IPayloadEntryValue, IRawTokenEntry, ITokenEntry} from '../inspect-correlation/contracts/index';
 
 @inject('InspectCorrelationService')
 export class TokenViewer {
