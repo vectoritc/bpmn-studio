6de74ec92805fdd17ab075953e272bf6fce6788e
Vyperus <bastian.schnittger@5minds.de>
2019-02-04 12:27:50 +0100

    :sparkles: Add Basic Functionallity to Modal

--- src/modules/solution-explorer/solution-explorer-solution/delete-diagram-modal/delete-diagram-modal.ts
+++ src/modules/solution-explorer/solution-explorer-solution/delete-diagram-modal/delete-diagram-modal.ts
@@ -1,3 +1,29 @@
-export class DeleteDiagramModal {
+import {IDiagram} from '@process-engine/solutionexplorer.contracts';
+import {ISolutionExplorerService} from '@process-engine/solutionexplorer.service.contracts';
 
+export class DeleteDiagramModal {
+  public showModal: boolean = false;
+
+  private _diagram: IDiagram;
+  private _service: ISolutionExplorerService;
+
+  public show(diagram: IDiagram, solutionService: ISolutionExplorerService): void {
+    this._diagram = diagram;
+    this._service = solutionService;
+
+    this.showModal = true;
+  }
+
+  public closeModal(): void {
+    this._diagram = undefined;
+    this._service = undefined;
+
+    this.showModal = false;
+  }
+
+  public async deleteDiagram(): Promise<void> {
+    await this._service.deleteDiagram(this._diagram);
+
+    this.showModal = false;
+  }
 }
