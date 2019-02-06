5038e9fe1607cd911ac03edfa8a65d3b50ec88c3
Vyperus <bastian.schnittger@5minds.de>
2019-02-06 11:23:35 +0100

    :truck: Move Error Handling to Modal

--- src/modules/solution-explorer/solution-explorer-solution/delete-diagram-modal/delete-diagram-modal.ts
+++ src/modules/solution-explorer/solution-explorer-solution/delete-diagram-modal/delete-diagram-modal.ts
@@ -1,28 +1,48 @@
+import {inject} from 'aurelia-framework';
+
 import {IDiagram} from '@process-engine/solutionexplorer.contracts';
 import {ISolutionExplorerService} from '@process-engine/solutionexplorer.service.contracts';
 
+import {NotificationType} from '../../../../contracts';
+import {NotificationService} from '../../../notification/notification.service';
+
+@inject('NotificationService')
 export class DeleteDiagramModal {
   public showModal: boolean = false;
 
   private _diagram: IDiagram;
-  private _service: ISolutionExplorerService;
+  private _solutionService: ISolutionExplorerService;
+  private _notificationService: NotificationService;
+
+  constructor(notificationService: NotificationService) {
+    this._notificationService = notificationService;
+  }
 
   public show(diagram: IDiagram, solutionService: ISolutionExplorerService): void {
     this._diagram = diagram;
-    this._service = solutionService;
+    this._solutionService = solutionService;
 
     this.showModal = true;
   }
 
   public closeModal(): void {
     this._diagram = undefined;
-    this._service = undefined;
+    this._solutionService = undefined;
 
     this.showModal = false;
   }
 
   public async deleteDiagram(): Promise<void> {
-    await this._service.deleteDiagram(this._diagram);
+    try {
+      await this._solutionService.deleteDiagram(this._diagram);
+    } catch (error) {
+      const message: string = `Unable to delete the diagram: ${error.message}`;
+
+      this._notificationService.showNotification(NotificationType.ERROR, message);
+    }
+
+    this._diagram = undefined;
+    this._solutionService = undefined;
 
     this.showModal = false;
   }
