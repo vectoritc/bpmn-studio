8a632793ad8a2a06f4469ee0e991000e8fd396ec
Vyperus <bastian.schnittger@5minds.de>
2019-02-07 16:24:46 +0100

    :sparkles: Use Correct Type For Login

--- src/contracts/authentication/IAuthenticationService.ts
+++ src/contracts/authentication/IAuthenticationService.ts
@@ -1,7 +1,8 @@
 import {IIdentity} from './IIdentity';
+import {ILoginResult} from './ILoginResult';
 
 export interface IAuthenticationService {
-  login(authority: string): Promise<void>;
+  login(authority: string): Promise<ILoginResult>;
   logout(authority: string): Promise<void>;
   isLoggedIn(authority: string): Promise<boolean>;
   getAccessToken(authority: string): Promise<string>;
