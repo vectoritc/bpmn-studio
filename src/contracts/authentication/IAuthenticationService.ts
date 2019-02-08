813727c45ef60517980b51839204a88463f3e704
Vyperus <bastian.schnittger@5minds.de>
2019-02-08 15:12:32 +0100

    :recycle: User Identity for Logout

--- src/contracts/authentication/IAuthenticationService.ts
+++ src/contracts/authentication/IAuthenticationService.ts
@@ -1,8 +1,9 @@
+import {IIdentity} from '@essential-projects/iam_contracts';
 import {ILoginResult} from './ILoginResult';
 
 export interface IAuthenticationService {
   login(authority: string): Promise<ILoginResult>;
-  logout(authority: string): Promise<void>;
+  logout(authority: string, identity: IIdentity): Promise<void>;
   isLoggedIn(authority: string): Promise<boolean>;
   getAccessToken(authority: string): Promise<string>;
 }
