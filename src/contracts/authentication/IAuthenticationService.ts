a3a93bf8f5c9394f218f9862f5309d145d52062b
Vyperus <bastian.schnittger@5minds.de>
2019-02-08 15:32:24 +0100

    :fire: Remove getAccessToken From Auth Service Interface

--- src/contracts/authentication/IAuthenticationService.ts
+++ src/contracts/authentication/IAuthenticationService.ts
@@ -5,5 +5,4 @@ export interface IAuthenticationService {
   login(authority: string): Promise<ILoginResult>;
   logout(authority: string, identity: IIdentity): Promise<void>;
   isLoggedIn(authority: string): Promise<boolean>;
-  getAccessToken(authority: string): Promise<string>;
 }
