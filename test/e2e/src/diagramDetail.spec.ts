6d2dd71e729a3b86f2d32e67a2bb8cf4997e40db
Vyperus <bastian.schnittger@5minds.de>
2019-02-12 17:27:19 +0100

    :sparkles: Add Diagram Detail Spec

---
+++ test/e2e/src/diagramDetail.spec.ts
@@ -0,0 +1,45 @@
+import {
+  browser,
+  ElementFinder,
+  protractor,
+  ProtractorExpectedConditions,
+} from 'protractor';
+
+import {SimpleDiagram} from './diagrams/simpleDiagram';
+import {DiagramDetail} from './pages/diagramDetail';
+import {RouterView} from './pages/routerView';
+
+describe('Diagram Detail', () => {
+
+  let diagramDetail: DiagramDetail;
+  let routerView: RouterView;
+  let diagram: SimpleDiagram;
+
+  const applicationUrl: string = browser.params.aureliaUrl;
+  const defaultTimeoutMS: number = browser.params.defaultTimeoutMS;
+
+  const expectedConditions: ProtractorExpectedConditions = protractor.ExpectedConditions;
+
+  beforeAll(async() => {
+    diagram = new SimpleDiagram();
+    diagramDetail = new DiagramDetail(applicationUrl, diagram.name);
+    routerView = new RouterView();
+
+    await diagram.deployDiagram();
+  });
+
+  afterAll(async() => {
+    await diagram.deleteDiagram();
+  });
+
+  beforeEach(async() => {
+    await routerView.init();
+    await diagramDetail.init();
+  });
+
+  it('should contain the bpmn-io container.', async() => {
+    const visibilityOfBpmnIoContainer: boolean = await diagramDetail.getVisibilityOfBpmnIoContainer();
+
+    expect(visibilityOfBpmnIoContainer).toBeTruthy();
+  });
+});
