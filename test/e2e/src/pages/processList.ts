7198359f633e5857ea91f4c13328d13b5cb02829
Vyperus <bastian.schnittger@5minds.de>
2019-02-12 17:21:13 +0100

    :sparkles: Add Refactored Process List Page

---
+++ test/e2e/src/pages/processList.ts
@@ -0,0 +1,88 @@
+import {browser, by, element, ElementFinder, ExpectedConditions} from 'protractor';
+
+import {By} from 'selenium-webdriver';
+
+export class ProcessList {
+
+  public url: string;
+
+  private _listEntryIdentifier: string = 'processList-';
+  private _processListContainerId: string = 'processListContainer';
+  private _diagramLinkClassName: string = 'process-list-item-modelname';
+  private _userTaskLinkClassName: string = 'process-list-item-user-tasks';
+
+  constructor(applicationUrl: string) {
+    this.url = `${applicationUrl}/process`;
+  }
+
+  public async init(): Promise<void> {
+    await browser.get(this.url);
+
+    browser.wait(ExpectedConditions.visibilityOf(this._processListContainer), browser.params.defaultTimeoutMS);
+  }
+
+  public async getVisibilityOfProcessListContainer(): Promise<boolean> {
+
+    return this._processListContainer.isDisplayed();
+  }
+
+  public async getVisibilityOfListEntry(correlationId: string): Promise<boolean> {
+    const listEntry: ElementFinder = this._getListEntry(correlationId);
+
+    return listEntry.isDisplayed();
+  }
+
+  public async getVisbilityOfDiagramDesignLink(correlationId: string): Promise<boolean> {
+    const diagramDesignLink: ElementFinder = this._getDiagramDesignLink(correlationId);
+
+    return diagramDesignLink.isDisplayed();
+  }
+
+  public async clickOnDiagramDesignLink(correlationId: string): Promise<void> {
+    const diagramDesignLink: ElementFinder = this._getDiagramDesignLink(correlationId);
+
+    return diagramDesignLink.click();
+  }
+
+  public async getVisbilityOfUserTaskLink(correlationId: string): Promise<boolean> {
+    const userTaskLink: ElementFinder = this._getUserTaskLink(correlationId);
+
+    return userTaskLink.isDisplayed();
+  }
+
+  public async clickOnUserTaskLink(correlationId: string): Promise<void> {
+    const userTaskLink: ElementFinder = this._getUserTaskLink(correlationId);
+
+    return userTaskLink.click();
+  }
+
+  private get _processListContainer(): ElementFinder {
+    const processListContainerbyId: By = by.id(this._processListContainerId);
+
+    return element(processListContainerbyId);
+  }
+
+  private _getListEntry(correlationId: string): ElementFinder {
+    const listEntryId: string = `${this._listEntryIdentifier}${correlationId}`;
+    const listEntryById: By = by.id(listEntryId);
+
+    return element(listEntryById);
+  }
+
+  private _getDiagramDesignLink(correlationId: string): ElementFinder {
+    const listEntryId: string = `${this._listEntryIdentifier}${correlationId}`;
+    const listEntryById: By = by.id(listEntryId);
+    const diagramLinkByTag: By = by.className(this._diagramLinkClassName);
+
+    return element(listEntryById).all(diagramLinkByTag).first();
+  }
+
+  private _getUserTaskLink(correlationId: string): ElementFinder {
+    const listEntryId: string = `${this._listEntryIdentifier}${correlationId}`;
+    const listEntryById: By = by.id(listEntryId);
+    const diagramLinkByTag: By = by.className(this._userTaskLinkClassName);
+
+    return element(listEntryById).all(diagramLinkByTag).first();
+  }
+
+}
