9b8926f99dd4a843acdfc2783d22f34a5084b6ae
Vyperus <bastian.schnittger@5minds.de>
2019-02-12 17:23:23 +0100

    :sparkles: Add new Private Getters

--- test/e2e/src/pages/solutionExplorer.ts
+++ test/e2e/src/pages/solutionExplorer.ts
@@ -4,6 +4,8 @@ import {By} from 'selenium-webdriver';
 
 export class SolutionExplorer {
 
+  private _diagramIdIdentifier: string = 'diagramList-';
+  private _solutionExplorerPanelTag: string = 'solution-explorer-panel';
 
 
   }
@@ -11,4 +13,18 @@ export class SolutionExplorer {
   }
 
   }
+
+  private get _solutionExplorerPanelContainer(): ElementFinder {
+    const panelContainerByTag: By = by.tagName(this._solutionExplorerPanelTag);
+
+    return element(panelContainerByTag);
+  }
+
+  private _getDiagramEntry(diagramName: string): ElementFinder {
+    const diagramEntryById: string = this._diagramIdIdentifier + diagramName;
+    const byId: By = by.id(diagramEntryById);
+
+    return element(byId);
+  }
+
 }
