19fc90070e437fe992f6dc875e9363dd3df6614c
Vyperus <bastian.schnittger@5minds.de>
2019-02-13 09:52:45 +0100

    :sparkles: Add Diff View Spec

---
+++ test/e2e/src/diffView.spec.ts
@@ -0,0 +1,61 @@
+import {
+  browser,
+  protractor,
+  ProtractorExpectedConditions,
+} from 'protractor';
+
+import {SimpleDiagram} from './diagrams/simpleDiagram';
+import {DiagramDetail} from './pages/diagramDetail';
+import {DiffView} from './pages/diffView';
+import {RouterView} from './pages/routerView';
+import {StatusBar} from './pages/statusBar';
+
+describe('Diff view', () => {
+
+  let routerView: RouterView;
+  let diagram: SimpleDiagram;
+  let statusBar: StatusBar;
+  let diagramDetail: DiagramDetail;
+  let diffView: DiffView;
+
+  const applicationUrl: string = browser.params.aureliaUrl;
+
+  beforeAll(async() => {
+    routerView = new RouterView();
+    diagram = new SimpleDiagram();
+    statusBar = new StatusBar();
+    diagramDetail = new DiagramDetail(applicationUrl, diagram.name);
+    diffView = new DiffView(applicationUrl, diagram.name);
+
+    await diagram.deployDiagram();
+  });
+
+  afterAll(async() => {
+
+    await diagram.deleteDiagram();
+  });
+
+  beforeEach(async() => {
+    await routerView.init();
+    await diagramDetail.init();
+  });
+
+  it('should contain `Show Diff` button in status bar.', async() => {
+    const statusBarDiffViewButtonIsDisplayed: boolean = await statusBar.getVisibilityOfEnableDiffViewButton();
+
+    expect(statusBarDiffViewButtonIsDisplayed).toBeTruthy();
+  });
+
+  it('should open diff view when click on `Show Diff` button.', async() => {
+    await statusBar.clickOnEnableDiffViewButton();
+
+    const currentBrowserUrl: string = await browser.getCurrentUrl();
+
+    expect(currentBrowserUrl).toContain(diffView.url);
+
+    const visibilityOfDiffViewContainer: boolean = await diffView.getVisibilityOfDiffViewContainer();
+
+    expect(visibilityOfDiffViewContainer).toBeTruthy();
+  });
+
+});
