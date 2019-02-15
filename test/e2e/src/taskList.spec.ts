7af6830b1960ea124c12fd04244c9ea3ba967029
Christian W <christian.werner@5minds.de>
2019-02-15 11:38:18 +0100

    💄Beautify Test Naming
    Co-Authored-By: Vyperus <bastian.schnittger@5minds.de>

--- test/e2e/src/taskList.spec.ts
+++ test/e2e/src/taskList.spec.ts
@@ -36,7 +36,7 @@ describe('Task List', () => {
     expect(visibilityOfListEntry).toBeTruthy();
   });
 
-  it('should navigate to design on click on link in table.', async() => {
+  it('should navigate to the `design view`, after clicking on the corresponding link in the table.', async() => {
     await taskList.clickOnDesignLink(diagram.name);
 
     const currentBrowserUrl: string = await browser.getCurrentUrl();
