a70bfbc8ca4c69830957e57fc105484f9b2d374f
Vyperus <bastian.schnittger@5minds.de>
2019-02-18 09:57:11 +0100

    :recycle: Add Visibility Check After Navigation

--- test/e2e/src/solutionExplorer.spec.ts
+++ test/e2e/src/solutionExplorer.spec.ts
@@ -1,6 +1,7 @@
 import {browser} from 'protractor';
 
 import {SimpleDiagram} from './diagrams/simpleDiagram';
+import {DiagramDetail} from './pages/diagramDetail';
 import {RouterView} from './pages/routerView';
 import {SolutionExplorer} from './pages/solutionExplorer';
 
@@ -9,12 +10,16 @@ describe('Solution Explorer', () => {
   let solutionExplorer: SolutionExplorer;
   let diagram: SimpleDiagram;
   let routerView: RouterView;
+  let diagramDetail: DiagramDetail;
+
+  const applicationUrl: string = browser.params.aureliaUrl;
 
   beforeAll(async() => {
 
     solutionExplorer = new SolutionExplorer();
     diagram = new SimpleDiagram();
     routerView = new RouterView();
+    diagramDetail = new DiagramDetail(applicationUrl, diagram.name);
 
     await diagram.deployDiagram();
   });
@@ -34,11 +39,15 @@ describe('Solution Explorer', () => {
     expect(diagramIsVisible).toBeTruthy();
   });
 
-  it('should navigate to the `design view`, after clicking on the diagram name.', async() => {
+  it('should navigate to the `detail view`, after clicking on the diagram name.', async() => {
     await solutionExplorer.openDiagramByClick(diagram.name);
 
     const currentBrowserUrl: string = await browser.getCurrentUrl();
 
     expect(currentBrowserUrl).toContain(diagram.name);
+
+    const visibilityOfDiagramDetailContainer: boolean = await diagramDetail.getVisibilityOfDiagramDetailContainer();
+
+    expect(visibilityOfDiagramDetailContainer).toBeTruthy();
   });
 });
